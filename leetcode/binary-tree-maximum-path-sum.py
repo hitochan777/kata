14f2ba65bec210d8472750f5dcd040bdd59e7b55
Otsuki Hitoshi <hitochan777@gmail.com>
2020-04-30 23:32:55 +0900

    binary tree maximum path sum

--- leetcode/binary-tree-maximum-path-sum.py
+++ leetcode/binary-tree-maximum-path-sum.py
@@ -4,21 +4,31 @@ class TreeNode:
         self.left = left
         self.right = right
 
-
 class Solution:
+    def __init__(self):
+        self.max = -10**100
+        
     def maxPathSum(self, root: TreeNode) -> int:
-        if root is None:
-            return 0
-        
-        self_max = self._maxPathSumPassingNode(root)
-        left_max = self.maxPathSum(root.left)
-        right_max = self.maxPathSum(root.right)
-        print(left_max, right_max, self_max)
-        
-        return max(left_max, right_max, self_max)
+        return self._maxPathSum(root)[0]
             
-    def _maxPathSumPassingNode(self, node: TreeNode) -> int:
-        if node is None:
-            return 0
+    def _maxPathSum(self, node: TreeNode):
+        self_max = node.val
+        candidates = []
+        line_candidates = [0]
         
-        return self._maxPathSumPassingNode(node.left) + self._maxPathSumPassingNode(node.right) + node.val
+        if node.left is not None:
+            left_max, left_line_max = self._maxPathSum(node.left)
+            self_max += max(0, left_line_max)
+            candidates.append(left_max)
+            line_candidates.append(left_line_max)
+        
+        if node.right is not None:
+            right_max, right_line_max = self._maxPathSum(node.right)
+            self_max += max(0, right_line_max)
+            candidates.append(right_max)
+            line_candidates.append(right_line_max)
+            
+        candidates.append(self_max)
+            
+        max_val = (max(candidates), max(line_candidates, default=0) + node.val)
+        return max_val
