9c8129def379b24aab4e1e9156599467efd00b99
hitochan777 <hitochan777@gmail.com>
2020-05-13 12:43:48 +0900

    Update single-element-in-a-sorted-array.py

--- leetcode/single-element-in-a-sorted-array.py
+++ leetcode/single-element-in-a-sorted-array.py
@@ -3,21 +3,20 @@ from typing import List
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
         l, h = 0, len(nums)
-        while l < h:
+        while l <= h:
             if l + 1 == h:
                 return nums[l]
-
             m = (l + h) // 2
             if m + 1 < len(nums) and nums[m + 1] == nums[m]:
                 if (len(nums) - m) % 2 == 0:
-                    l = m + 1
-                else:
                     h = m - 1
+                else:
+                    l = m + 1
             elif m - 1 >= 0 and nums[m-1] == nums[m]:
                 if m % 2 == 0:
-                    l = m + 1
+                    h = m - 1
                 else:
-                    h = m - 1       
+                    l = m + 1
             else:
                 return nums[m]
 
@@ -26,5 +25,15 @@ class Solution:
 
 if __name__ == "__main__":
     solver = Solution()
-    assert solver.singleNonDuplicate([1,1,2,3,3,4,4,8,8]) == 2
-    assert solver.singleNonDuplicate([3,3,7,7,10,11,11]) == 10 
+    actual = solver.singleNonDuplicate([1,1,2,2,4,4,5,5,9])
+    assert actual == 9, actual
+    actual = solver.singleNonDuplicate([1,1,2,3,3,4,4,8,8])
+    assert actual == 2, actual
+    actual = solver.singleNonDuplicate([3,3,7,7,10,11,11])
+    assert actual == 10, actual
+    actual = solver.singleNonDuplicate([3,3,7])
+    assert actual == 7, actual
+    actual = solver.singleNonDuplicate([7])
+    assert actual == 7, actual
+    actual = solver.singleNonDuplicate([2,3,3])
+    assert actual == 2, actual
