70985c8911e1ca6f54d36a03fd8f6ca12ef1c22a
Otsuki Hitoshi <hitochan777@gmail.com>
2020-06-17 08:24:39 +0900

    validate-ip-address

--- leetcode/validate-ip-address.py
+++ leetcode/validate-ip-address.py
@@ -1,15 +1,13 @@
 import re
-
 class Solution:
-    def is_ipv4(self, IP: str) -> bool:
-        parts = IP.split(".")
-        if len(parts) != 4:
-            return False
-        
-        
-    def is_ipv6(self, IP: str) -> bool:
-        return False
-        
-    def validIPAddress(self, IP: str) -> str:
-        
-        
+
+    def validIPAddress(self, IP: str) -> str:  
+        chunk_IPv4 = r'([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])'
+        patten_IPv4 = re.compile(r'^(' + chunk_IPv4 + r'\.){3}' + chunk_IPv4 + r'$')
+        chunk_IPv6 = r'([0-9a-fA-F]{1,4})'
+        patten_IPv6 = re.compile(r'^(' + chunk_IPv6 + r'\:){7}' + chunk_IPv6 + r'$')
+
+        if patten_IPv4.match(IP):
+            return "IPv4"
+
+        return "IPv6" if patten_IPv6.match(IP) else "Neither" 
