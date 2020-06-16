9ad0fc2d910b2e333535df4b895624e24c1178e2
Otsuki Hitoshi <hitochan777@gmail.com>
2020-06-16 20:57:21 +0900

    wip

---
+++ leetcode/validate-ip-address.py
@@ -0,0 +1,15 @@
+import re
+
+class Solution:
+    def is_ipv4(self, IP: str) -> bool:
+        parts = IP.split(".")
+        if len(parts) != 4:
+            return False
+        
+        
+    def is_ipv6(self, IP: str) -> bool:
+        return False
+        
+    def validIPAddress(self, IP: str) -> str:
+        
+        
