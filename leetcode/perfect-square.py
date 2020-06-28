608a10012516d5ea6a11145fb53e36bde1cd92ca
Otsuki Hitoshi <hitochan777@gmail.com>
2020-06-28 19:08:53 +0900

    perfect square

--- leetcode/perfect-square.py
+++ leetcode/perfect-square.py
@@ -4,9 +4,9 @@ class Solution:
         root = int(math.sqrt(n))
         for i in range(1, n+1):
             for j in range(1, root+1):
-                if i == j**2:
+                if i == j * j:
                     dp[i] = 1
-                elif i > j**2:
-                    dp[i] = min(dp[i], dp[i - j**2] + 1)
+                elif i > j * j:
+                    dp[i] = min(dp[i], dp[i - j * j] + 1)
                     
         return dp[n]
