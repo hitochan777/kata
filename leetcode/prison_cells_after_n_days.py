e75ed6d318346588501e06afb95b07e71ef0f66f
hitochan777 <hitochan777@gmail.com>
2020-07-05 16:28:59 +0900

    Update prison_cells_after_n_days.py

--- leetcode/prison_cells_after_n_days.py
+++ leetcode/prison_cells_after_n_days.py
@@ -1,7 +1,11 @@
 class Solution:
     def prisonAfterNDays(self, cells: List[int], N: int) -> List[int]:
+        if N>14:
+            N = N%14 + 14
+        
         for n in range(N):
             next_cells = cells[:]
+            next_cells[0] = next_cells[-1] = 0
             for i in range(1, len(cells)-1):
                 if cells[i-1] == cells[i+1]:
                     next_cells[i] = 1
