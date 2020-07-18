669117df49bc3d02082dd44abe05b1356db03f7b
hitochan777 <hitochan777@gmail.com>
2020-07-18 23:58:46 +0900

    Delete top-k-frequent.py

--- leetcode/top-k-frequent.py
+++
@@ -1,4 +0,0 @@
-class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        most_commons = Counter(nums).most_common(k)
-        return list(map(lambda x: x[0], most_commons))
