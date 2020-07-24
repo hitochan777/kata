48d127a271a03d166d586611ea19ac83b2616877
Otsuki Hitoshi <hitochan777@gmail.com>
2020-07-24 22:35:45 +0900

    WIP

---
+++ leetcode/csharp/src/AllPathsFromSourceToTarget.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AllPathsFromSourceToTarget
+{
+    public class Solution
+    {
+        public IList<IList<int>> findPaths(int[][] graph, int current, int target)
+        {
+            if (current > target)
+            {
+                return new List<List<int>> {3};
+            }
+            var paths = new List<List<int>>();
+            for (int i = 0; i < graph[current].Length; i++)
+            {
+                paths.AddRange(findPaths(graph, graph[current][i], target));
+            }
+            foreach (var path in paths)
+            {
+                path.Insert(0, current);
+            }
+            return paths;
+        }
+        public IList<IList<int>> AllPathsSourceTarget(int[][] graph)
+        {
+
+            return findPaths(graph, 0, graph.Length - 1);
+        }
+    }
+}
