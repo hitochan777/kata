06d96d13e767f338efd82e0fe2b0228fba8b184a
Otsuki Hitoshi <hitochan777@gmail.com>
2020-07-24 23:49:23 +0900

    AllPathsFromSourceToTarget

--- leetcode/csharp/src/AllPathsFromSourceToTarget.cs
+++ leetcode/csharp/src/AllPathsFromSourceToTarget.cs
@@ -5,13 +5,13 @@ namespace AllPathsFromSourceToTarget
 {
     public class Solution
     {
-        public IList<IList<int>> findPaths(int[][] graph, int current, int target)
+        public List<IList<int>> findPaths(int[][] graph, int current, int target)
         {
-            if (current > target)
+            if (current == target)
             {
-                return new List<List<int>> {3};
+                return new List<IList<int>> { new List<int> { target } };
             }
-            var paths = new List<List<int>>();
+            var paths = new List<IList<int>>();
             for (int i = 0; i < graph[current].Length; i++)
             {
                 paths.AddRange(findPaths(graph, graph[current][i], target));
