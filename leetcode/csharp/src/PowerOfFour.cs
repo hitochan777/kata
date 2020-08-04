bc64305f6fd298b6697bbe410042d742daed75b2
hitochan777 <hitochan777@gmail.com>
2020-08-04 21:23:55 +0900

    Create PowerOfFour.cs

---
+++ leetcode/csharp/src/PowerOfFour.cs
@@ -0,0 +1,53 @@
+public class Solution {
+    public bool IsPowerOfFour(int num) {
+        if (num == 1) {
+            return true;
+        }
+        if (num == 4) {
+            return true;
+        }
+        if (num == 16) {
+            return true;
+        }
+        if (num == 64) {
+            return true;
+        }
+        if (num == 256) {
+            return true;
+        }
+        if (num == 1024) {
+            return true;
+        }
+        if (num == 4096) {
+            return true;
+        }
+        if (num == 16384) {
+            return true;
+        }
+        if (num == 65536) {
+            return true;
+        }
+        if (num == 262144) {
+            return true;
+        }
+        if (num == 1048576) {
+            return true;
+        }
+        if (num == 4194304) {
+            return true;
+        }
+        if (num == 16777216) {
+            return true;
+        }
+        if (num == 67108864) {
+            return true;
+        }
+        if (num == 268435456) {
+            return true;
+        }
+        if (num == 1073741824) {
+            return true;
+        }
+        return false;
+    }
+}
