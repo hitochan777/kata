d1dee1a2925b5eb709775933250407bfe9f5c717
hitochan777 <hitochan777@gmail.com>
2020-08-04 21:30:53 +0900

    Update PowerOfFour.cs

--- leetcode/csharp/src/PowerOfFour.cs
+++ leetcode/csharp/src/PowerOfFour.cs
@@ -1,53 +1,9 @@
 public class Solution {
     public bool IsPowerOfFour(int num) {
-        if (num == 1) {
-            return true;
+        if (num == 0) {
+            return false;
         }
-        if (num == 4) {
-            return true;
-        }
-        if (num == 16) {
-            return true;
-        }
-        if (num == 64) {
-            return true;
-        }
-        if (num == 256) {
-            return true;
-        }
-        if (num == 1024) {
-            return true;
-        }
-        if (num == 4096) {
-            return true;
-        }
-        if (num == 16384) {
-            return true;
-        }
-        if (num == 65536) {
-            return true;
-        }
-        if (num == 262144) {
-            return true;
-        }
-        if (num == 1048576) {
-            return true;
-        }
-        if (num == 4194304) {
-            return true;
-        }
-        if (num == 16777216) {
-            return true;
-        }
-        if (num == 67108864) {
-            return true;
-        }
-        if (num == 268435456) {
-            return true;
-        }
-        if (num == 1073741824) {
-            return true;
-        }
-        return false;
+        int pow = (int)(Math.Log(num) / Math.Log(4));
+        return Math.Pow(4, pow) == num;
     }
 }
