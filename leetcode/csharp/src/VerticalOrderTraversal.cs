650a5853712d03e8f7961f878d3aaf4ceb4311d7
hitochan777 <hitochan777@gmail.com>
2020-08-08 12:54:37 +0900

    Update VerticalOrderTraversal.cs

--- leetcode/csharp/src/VerticalOrderTraversal.cs
+++ leetcode/csharp/src/VerticalOrderTraversal.cs
@@ -1,36 +1,29 @@
 public class Solution {
-	private void mergeDictionary(Dictionary<int, IList<int>> a, Dictionary<int, IList<int>> b) {
-		foreach (var item in b) {
-			if (a.ContainsKey(item.Key)){
-				a[item.Key] = a[item.Key].Concat(item.Value).ToList();
-			} else {
-				a[item.Key] = item.Value;
-			}
-		}
-	}
-	
-	private Dictionary<int, IList<int>> traversal(TreeNode node, int index) {
-		if (node == null) {
-			return new Dictionary<int, IList<int>>();
-		}
-		var dict = new Dictionary<int, IList<int>>()
-		  {
-			{ index, new List<int> {node.val}}
-		  };
-		var leftDict = traversal(node.left, index - 1);
-		var rightDict = traversal(node.right, index + 1);
-		mergeDictionary(dict, leftDict);
-		mergeDictionary(dict, rightDict);
-		return dict;
-	}
-    public IList<IList<int>> VerticalTraversal(TreeNode root) {
-       	var dict = traversal(root, 0);
-		var keys = dict.Keys.ToList();
-		keys.Sort();
-		IList<IList<int>> result = new List<IList<int>> {};
-		foreach (var key in keys) {
-			result.Add(dict[key]);
-		}
-		return result;
+  public IList < IList < int >> VerticalTraversal(TreeNode root) {
+    var dict = new Dictionary < int,
+    List < (int Y, int val) >> ();
+    var queue = new Queue < (int X, int Y, TreeNode node) > ();
+    queue.Enqueue((0, 0, root));
+    while (queue.Count > 0) {
+      var node = queue.Dequeue();
+      if (node.node == null) {
+        continue;
+      }
+      if (!dict.ContainsKey(node.X)) {
+        dict[node.X] = new List < (int Y, int val) > ();
+      }
+      dict[node.X].Add((node.Y, node.node.val));
+      queue.Enqueue((node.X - 1, node.Y + 1, node.node.left));
+      queue.Enqueue((node.X + 1, node.Y + 1, node.node.right));
     }
+    var keys = dict.Keys.ToList();
+    keys.Sort();
+    IList < IList < int >> result = new List < IList < int >> {};
+    foreach(var key in keys) {
+      var list = dict[key];
+      list.Sort();
+      result.Add(list.Select(item =>item.val).ToList());
+    }
+    return result;
+  }
 }
