b88afbbf5e6bae5c2547d37a7d9ee65164539ee3
hitochan777 <hitochan777@gmail.com>
2020-08-13 00:04:23 +0900

    Update pascals-triangle-2.py

--- leetcode/pascals-triangle-2.py
+++ leetcode/pascals-triangle-2.py
@@ -1,7 +1,6 @@
 class Solution:
-    def getRow(self, rowIndex: int) -> List[int]:
-        li = [1]
-        for i in range(rowIndex):
-            pass
+    def n_choose_k(self, n, k):
+        return factorial(n) // (factorial(k) * factorial(n-k))
         
-        return li
+    def getRow(self, rowIndex: int) -> List[int]:
+        return [self.n_choose_k(rowIndex, i) for i in range(rowIndex+1)]
