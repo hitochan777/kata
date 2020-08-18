fd4f0837d941e1a348574bbaf60442ab977bda99
Otsuki Hitoshi <hitochan777@gmail.com>
2020-08-18 23:02:28 +0900

    NumbersWithSameConsecutiveDifferences

---
+++ leetcode/csharp/src/NumbersWithSameConsecutiveDifferences.cs
@@ -0,0 +1,37 @@
+using System.Linq;
+using System.Collections.Generic;
+
+namespace src
+{
+	public class NumbersWithSameConsecutiveDifferences
+	{
+		private int getLastDigit(int num)
+		{
+			return num % 10;
+		}
+
+		private void TryAdd(int partialNum, int modifiedLastDigit, List<int> nums)
+		{
+			if (modifiedLastDigit >= 0 && modifiedLastDigit <= 9)
+			{
+				nums.Add(int.Parse(partialNum.ToString() + modifiedLastDigit.ToString()));
+			}
+		}
+
+		public int[] NumsSameConsecDiff(int N, int K) {
+			if (N == 1) {
+				return new int[]{0,1,2,3,4,5,6,7,8,9};
+			}
+
+			var nums = new List<int>();
+			var partialNums = NumsSameConsecDiff(N - 1, K);
+			foreach (int partialNum in partialNums)
+			{
+				int lastDigit = getLastDigit(partialNum);
+				TryAdd(partialNum, lastDigit + K, nums);
+				TryAdd(partialNum, lastDigit - K, nums);
+			}
+			return nums.Where(num => num.ToString().Length == N).Distinct().ToArray();
+		}
+	}
+}
---
+++ leetcode/csharp/test/NumbersWithSameConsecutiveDifferencesTest.cs
@@ -0,0 +1,42 @@
+using NUnit.Framework;
+using src;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace test
+{
+	public class NumbersWithSameConsecutiveDifferencesTest
+	{
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+		[Test]
+		public void Test1()
+		{
+			var solver = new NumbersWithSameConsecutiveDifferences();
+			var actual = solver.NumsSameConsecDiff(3, 7).ToHashSet();
+			var expected = new HashSet<int> {181, 292, 707, 818, 929};
+			Assert.IsTrue(expected.SetEquals(actual));
+		}
+
+		[Test]
+		public void Test2()
+		{
+			var solver = new NumbersWithSameConsecutiveDifferences();
+			var actual = solver.NumsSameConsecDiff(2, 1).ToHashSet();
+			var expected = new HashSet<int> {10, 12, 21, 23, 32, 34, 43, 45, 54, 56, 65, 67, 76, 78, 87, 89, 98};
+			Assert.IsTrue(expected.SetEquals(actual));
+		}
+
+		[Test]
+		public void Test3()
+		{
+			var solver = new NumbersWithSameConsecutiveDifferences();
+			var actual = solver.NumsSameConsecDiff(2, 0).ToHashSet();
+			var expected = new HashSet<int> {11, 22, 33, 44, 55, 66, 77, 88, 99};
+			Assert.IsTrue(expected.SetEquals(actual));
+		}
+	}
+}
