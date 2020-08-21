473d5d9bde19ff2e4f3c0331830ce67d017f469a
Otsuki Hitoshi <hitochan777@gmail.com>
2020-08-21 09:04:05 +0900

    WIP: ReorderList

---
+++ leetcode/csharp/src/ReorderList.cs
@@ -0,0 +1,78 @@
+namespace src
+{
+	public class ListNode
+	{
+		public int val;
+		public ListNode next;
+
+		public ListNode(int val = 0, ListNode next = null)
+		{
+			this.val = val;
+			this.next = next;
+		}
+	}
+
+	public class ListReorderer
+	{
+		private ListNode reverseList(ListNode head)
+		{
+			if (head == null)
+			{
+				return null;
+			}
+
+			ListNode cur = head.next;
+			ListNode pre = head;
+			while (cur != null)
+			{
+				ListNode tmp = cur.next;
+				cur.next = pre;
+				pre = cur;
+				cur = tmp;
+			}
+			head.next = null;
+
+			return pre;
+		}
+
+		private ListNode breakListInMiddle(ListNode head)
+		{
+			if (head?.next == null)
+			{
+				return null;
+			}
+
+			ListNode slow = head, fast = head.next;
+			while (fast != null)
+			{
+				slow = slow.next;
+				fast = fast.next;
+			}
+
+			ListNode secondHead = slow.next;
+			slow.next = null;
+			return secondHead;
+		}
+
+		private void mergedLists(ListNode first, ListNode second)
+		{
+			while (first != null)
+			{
+				ListNode tmp1 = first.next;
+				ListNode tmp2 = second.next;
+				first.next = second;
+				second.next = tmp1;
+
+				first = tmp1;
+				second = tmp2;
+			}
+		}
+
+		public void ReorderList(ListNode head)
+		{
+			ListNode latterHead =  breakListInMiddle(head);
+			ListNode reversedLatterHead = reverseList(latterHead);
+			mergedLists(head, reversedLatterHead);
+		}
+	}
+}
