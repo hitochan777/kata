473777e2ffdbada4aba79227be3eccd8918e302a
Otsuki Hitoshi <hitochan777@gmail.com>
2020-08-21 23:22:57 +0900

    sort array by parity

---
+++ leetcode/csharp/src/SortArrayByParity.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+
+namespace src
+{
+	public class ParityComparer : IComparer
+	{
+		public int Compare(Object x, Object y)
+		{
+			return (int) x % 2 - (int) y % 2;
+		}
+	}
+
+	public class SortArrayByParity
+	{
+		public int[] Solve(int[] A)
+		{
+			Array.Sort(A, new ParityComparer());
+			return A;
+		}
+	}
+}
