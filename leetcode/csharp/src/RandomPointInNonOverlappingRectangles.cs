b0147d78b57dc1026897f463986ee08f1611a156
Otsuki Hitoshi <hitochan777@gmail.com>
2020-08-22 23:45:31 +0900

    WIP: random point in non-overlapping rectangles

---
+++ leetcode/csharp/src/RandomPointInNonOverlappingRectangles.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Linq;
+
+namespace src
+{
+	public class RandomPointInNonOverlappingRectangles
+	{
+		private int[][] Rects;
+		private int[] Areas;
+		public RandomPointInNonOverlappingRectangles(int[][] rects)
+		{
+			this.Rects = rects;
+			this.Areas = rects.Select(rect => Math.Abs(rect[0] - rect[2]) * Math.Abs(rect[1] - rect[3])).ToArray();
+		}
+
+		private int SampleIndex(int[] weights)
+		{
+			var random = new Random();
+			double randomValue = (int)(random.NextDouble() * weights.Sum());
+			int current = 0;
+			for (int i = 0; i < weights.Length; i++)
+			{
+				int weight = weights[i];
+				if (randomValue >= current && randomValue < current + weight)
+				{
+					return i;
+				}
+				current += weight;
+			}
+
+			return weights.Length - 1;
+		}
+
+		public int[] Pick()
+		{
+			var random = new Random();
+			int index = SampleIndex(this.Areas);
+			var rand1 = random.NextDouble();
+			var rand2 = random.NextDouble();
+
+		}
+	}
+}
