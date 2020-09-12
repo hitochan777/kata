80214ed53d2ad76c23e841681cbd88e0ea766031
Otsuki Hitoshi <hitochan777@gmail.com>
2020-09-12 23:58:40 +0900

    CombinationSum3

---
+++ leetcode/csharp/src/CombinationSum3.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Transactions;
+
+namespace src
+{
+	public class CombinationSum3
+	{
+		public class Solution {
+			private void CombinationSum3(IList<IList<int>> result,  IList<int> cur, int k, int start, int sum)
+			{
+				if (sum < 0)
+				{
+					return;
+				}
+
+				if (sum == 0 && cur.Count == k)
+				{
+					result.Add(cur);
+					return;
+				}
+
+				for (int i = start; i <= 9; i++)
+				{
+					cur.Add(i);
+					CombinationSum3(result, cur, k, i + 1, sum - i);
+					cur.Remove(cur.Count - 1);
+				}
+			}
+
+			public IList<IList<int>> CombinationSum3(int k, int n)
+			{
+				var result = new List<IList<int>>();
+				var cur = new List<int>();
+				CombinationSum3(result, cur, k, 1, n);
+				return result;
+			}
+        }
+	}
+}
