e9519f798537c8ca8c6a7168ce3857f8c81352d8
hitochan777 <hitochan777@gmail.com>
2020-10-07 09:28:18 +0900

    Create ComplementOfBase10Integer.cs

---
+++ leetcode/csharp/src/ComplementOfBase10Integer.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace src {
+    public class ComplementOfBase10Integer.cs {
+        public int BitwiseComplement(int N) {
+            var n = Convert.ToString(N, 2).Length;
+            return ~N & ((1 << n) - 1);
+        }
+    }
+}
