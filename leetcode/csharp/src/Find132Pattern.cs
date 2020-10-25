3c92bafdf2e86b0bd8470f9f22edb0fc0e3a910f
Otsuki Hitoshi <hitochan777@gmail.com>
2020-10-25 11:26:55 +0900

    format

--- leetcode/csharp/src/Find132Pattern.cs
+++ leetcode/csharp/src/Find132Pattern.cs
@@ -1,31 +1,41 @@
 using System.Collections.Generic;
 
-namespace src {
-    class Find132Pattern {
-        public bool Solve(int[] nums) {
-            var stack = new Stack<(int, int)>();
-            foreach (var num in nums) {
-                if (stack.Count == 0 || num < stack.Peek().Item1) {
-                    stack.Push((num, num));
-                }
-                else {
-                    var min  = stack.Peek().Item1;
-                    while (stack.Count > 0 && stack.Peek().Item2 < num) {
-                        stack.Pop();
-                    }
-                    (int, int)? top = null;
-                    if (stack.Count > 0) {
-                        top = stack.Peek();
-                    } 
-                    if (top?.Item1 < num && top?.Item2 > num) {
-                        return true;
-                    }
-                    else {
-                        stack.Push((min, num));
-                    }
-                }
-            }
-            return false;
-        }
-    }
+namespace src
+{
+	class Find132Pattern
+	{
+		public bool Solve(int[] nums)
+		{
+			var stack = new Stack<(int, int)>();
+			foreach (var num in nums)
+			{
+				if (stack.Count == 0 || num < stack.Peek().Item1)
+				{
+					stack.Push((num, num));
+				}
+				else
+				{
+					var min = stack.Peek().Item1;
+					while (stack.Count > 0 && stack.Peek().Item2 < num)
+					{
+						stack.Pop();
+					}
+					(int, int)? top = null;
+					if (stack.Count > 0)
+					{
+						top = stack.Peek();
+					}
+					if (top?.Item1 < num && top?.Item2 > num)
+					{
+						return true;
+					}
+					else
+					{
+						stack.Push((min, num));
+					}
+				}
+			}
+			return false;
+		}
+	}
 }
