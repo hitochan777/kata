01b5e5fe90b8b99b37a3289821e27e298444a8c4
Otsuki Hitoshi <hitochan777@gmail.com>
2021-02-09 00:04:36 +0900

    ABC159 D

--- atcoder/abc159/D.py
+++ atcoder/abc159/D.py
@@ -3,12 +3,9 @@ n = int(input())
 nums = input().split()
 c = Counter(nums)
 
+total = sum(v * (v - 1) // 2 for k, v in c.items()) 
+
 for num in nums:
-    c[num] -= 1
-    total = 0
-    for k, v in c.items():
-        total += v * (v - 1) // 2
-
-    c[num] += 1
-    print(total)
-
+    val = c[num]
+    adjusted = total - val * (val - 1) // 2 + (val - 1) * (val - 2) // 2
+    print(adjusted)
