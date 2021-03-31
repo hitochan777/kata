7b0645a86a54e784187fc0d5a163352dd5d3477a
Otsuki Hitoshi <hitochan777@gmail.com>
2021-03-31 22:13:25 +0900

    ABC197 D

--- atcoder/abc197/D.py
+++ atcoder/abc197/D.py
@@ -8,6 +8,5 @@ cx, cy = (xh + x0) / 2, (yh + y0) / 2
 
 theta = math.atan2(y0 - cy, x0 - cx) + (2 * math.pi) / N
 r = math.sqrt((xh - x0)**2 + (yh - y0)**2) / 2
-print(math.sin(theta))
-x1, y1 = r * math.cos(theta), r * math.sin(theta)
+x1, y1 = cx + r * math.cos(theta), cy + r * math.sin(theta)
 print(x1, y1)
