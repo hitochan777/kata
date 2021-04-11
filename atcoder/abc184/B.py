a856add409f64a96b889d4b90e97b330db5125d4
Otsuki Hitoshi <hitochan777@gmail.com>
2021-04-11 13:50:50 +0900

    ABC184 B

--- atcoder/abc184/B.py
+++ atcoder/abc184/B.py
@@ -3,7 +3,12 @@ from collections import Counter
 N, X = (int(x) for x in input().split())
 S = input()
 
-cnt = Counter(S)
-score = max(0, X - cnt["x"] + cnt["o"])
+score = X
+for s in S:
+  if s == "x":
+    score = max(0, score - 1)
+  else:
+    score += 1
+
 print(score)
 
