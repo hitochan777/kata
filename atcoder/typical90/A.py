bd56e840ad64dd34bbd8ca0310e379fc54af0cd7
Otsuki Hitoshi <hitochan777@gmail.com>
2021-06-17 22:52:48 +0900

    typical90 A

--- atcoder/typical90/A.py
+++ atcoder/typical90/A.py
@@ -2,17 +2,26 @@ N, L = (int(x) for x in input().split())
 K = int(input())
 A = list(int(x) for x in input().split())
 
-def is_divisible(splits, m) -> bool:
-  return False
+def calc_divisible_count(splits, m, l) -> int:
+  splits = [0] + splits + [l]
+  n = len(splits)
+  cur = 0
+  cnt = 0
+  for i in range(1, n):
+    cur += splits[i]  - splits[i-1]
+    if cur >= m:
+      cur = 0
+      cnt += 1
+
+  # print(splits, m, l, cnt)
+  return cnt
 
 l, h = 0, L
-while l < h:
-  m = (l + h) >> 2:
-  if is_divisible(A, m):
+while l <= h:
+  m = (l + h) >> 1
+  if calc_divisible_count(A, m, L) >= K + 1:
     l = m + 1
   else:
     h = m - 1
 
-print(m)
-
-
+print(h)
