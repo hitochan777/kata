9baf3864d8c650df90a75ddde550b1b45123669d
Otsuki Hitoshi <hitochan777@gmail.com>
2021-06-23 00:25:57 +0900

    typical90 D

--- atcoder/typical90/D.py
+++ atcoder/typical90/D.py
@@ -1,15 +1,31 @@
 import numpy as np
+import sys
 
 H, W = list(int(x) for x in input().split())
 
+def readline():
+  return sys.stdin.readline()
+
+def writeline(s: str):
+  sys.stdout.write(s + "\n")
+
 matrix = []
 for _ in range(H):
-  li = list(int(x) for x in input().split())
+  li = list(int(x) for x in readline().split())
   matrix.append(li)
 
-s1 = np.sum(matrix, axis=1)
-s2 = np.sum(matrix, axis=0)
+matrix = np.array(matrix)
 
-ans = [[str(s1[i] + s2[j] - matrix[i][j]) for i in range(H)] for j in range(W)]
-for li in ans:
-  print(" ".join(li))
+s1 = np.sum(matrix, axis=1)
+s1 = np.tile(s1, (W, 1))
+s1 = s1.transpose()
+s2 = np.sum(matrix, axis=0)
+s2 = np.tile(s2, (H, 1))
+
+rows = s1 + s2 - matrix
+for row in rows:
+  writeline(" ".join(map(str, row)))
+
+# ans = [[str(s1[i] + s2[j] - matrix[i][j]) for j in range(W)] for i in range(H)]
+# for li in ans:
+#   writeline(" ".join(li))
--- atcoder/typical90/D_random_generator.py
+++ atcoder/typical90/D_random_generator.py
@@ -1,6 +1,8 @@
 import random
-H, W = 2000, 2000
+H, W = random.randint(1, 2000), random.randint(1, 2000)
 print(H, W)
 for _ in range(H):
   for _ in range(W):
     print(random.randint(1, 99), end=" ")
+
+  print("")
