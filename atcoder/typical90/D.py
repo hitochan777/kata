97866d582803ac0d1315ec04b6af9797fa35d41c
Otsuki Hitoshi <hitochan777@gmail.com>
2021-06-23 00:43:44 +0900

    typical90 D

--- atcoder/typical90/D.py
+++ atcoder/typical90/D.py
@@ -1,31 +1,19 @@
-import numpy as np
-import sys
-
 H, W = list(int(x) for x in input().split())
-
-def readline():
-  return sys.stdin.readline()
-
-def writeline(s: str):
-  sys.stdout.write(s + "\n")
 
 matrix = []
 for _ in range(H):
-  li = list(int(x) for x in readline().split())
+  li = list(int(x) for x in input().split())
   matrix.append(li)
 
-matrix = np.array(matrix)
+s1 = []
+for row in matrix:
+  s1.append(sum(row))
 
-s1 = np.sum(matrix, axis=1)
-s1 = np.tile(s1, (W, 1))
-s1 = s1.transpose()
-s2 = np.sum(matrix, axis=0)
-s2 = np.tile(s2, (H, 1))
+s2 = []
+for j in range(W):
+  s = sum(matrix[i][j] for i in range(H))
+  s2.append(s)
 
-rows = s1 + s2 - matrix
-for row in rows:
-  writeline(" ".join(map(str, row)))
-
-# ans = [[str(s1[i] + s2[j] - matrix[i][j]) for j in range(W)] for i in range(H)]
-# for li in ans:
-#   writeline(" ".join(li))
+ans = [[str(s1[i] + s2[j] - matrix[i][j]) for j in range(W)] for i in range(H)]
+for li in ans:
+  print(" ".join(li))
