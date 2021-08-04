4e15acb754ce14863d7644d171b6a681052f8623
Otsuki Hitoshi <hitochan777@gmail.com>
2021-08-04 21:39:01 +0900

    improve ABC212 C

--- atcoder/abc212/C.py
+++ atcoder/abc212/C.py
@@ -8,14 +8,9 @@ ans = 10**18
 while pa < N and pb < M:
   a, b = A[pa], B[pb]
   ans = min(abs(a - b), ans)
-  if ans == 0:
-    break
-
   if a < b:
     pa += 1
   else:
     pb += 1
 
 print(ans)
-# 3 8 10 21
-# 5 7 20
