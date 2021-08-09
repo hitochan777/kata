8fa0a664f78a59d87267738314d84c6a3d37f4cf
Otsuki Hitoshi <hitochan777@gmail.com>
2021-08-09 22:51:31 +0900

    ABC212 E

--- atcoder/abc212/E.py
+++ atcoder/abc212/E.py
@@ -1,11 +1,11 @@
 from collections import defaultdict
 
-N, M , K = (int(x) for x in input().split())
+N, M, K = (int(x) for x in input().split())
 graph = defaultdict(list)
 for _ in range(M):
   U, V = (int(x) for x in input().split())
-  graph[U].append(V)
-  graph[V].append(U)
+  graph[U-1].append(V-1)
+  graph[V-1].append(U-1)
 
 dp = [[0] * N for _ in range(K+1)]
 dp[0][0] = 1
@@ -13,7 +13,7 @@ dp[0][0] = 1
 for day in range(K):
   s = sum(dp[day])
   for nxt in range(N):
-    dp[day+1][nxt] = s - dp[day][nxt]
-    dp[day+1][nxt] = s - sum(dp[day][node] for node in graph[nxt])
+    dp[day+1][nxt] = (s - dp[day][nxt] - sum(dp[day][node] for node in graph[nxt])) % 998244353
+
 
 print(dp[K][0])
