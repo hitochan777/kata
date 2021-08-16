36a01c5273c015cb1111bef2b8b9f5b2de4c9b48
Otsuki Hitoshi <hitochan777@gmail.com>
2021-08-17 00:09:29 +0900

    Typical90 C

--- atcoder/typical90/C.py
+++ atcoder/typical90/C.py
@@ -1,24 +1,34 @@
-from collections import defauldict
+from collections import defaultdict
 
-graph = defauldict(list)
+graph = defaultdict(list)
 
 N = int(input())
-for _ in range(N):
+for _ in range(N-1):
   A, B = (int(x) for x in input().split())
   A, B = A-1, B-1
-  graph[A] = B
-  graph[B] = A
+  graph[A].append(B)
+  graph[B].append(A)
 
-def get_max_dist(node, graph, visited, dist):
+def get_max_dist(node, graph):
+  visited = set()
+  return _get_max_dist(node, graph, visited, 0)
+
+def _get_max_dist(node, graph, visited, dist):
   if node in visited:
-    return dist - 1
+    return dist - 1, None
 
   visited.add(node)
   max_dist = dist
+  max_node = node
   for nb in graph[node]:
-    cur_dist = get_max_dist(nb, graph, visited, dist+1)
-    max_dist = max(max_dist, cur_dist)
+    cur_dist, cur_node = _get_max_dist(nb, graph, visited, dist+1)
+    if cur_dist > max_dist:
+      max_dist = cur_dist
+      max_node = cur_node
 
-  return max_dist
+  return max_dist, max_node
 
-get_max_dist(0)
+
+dist, node = get_max_dist(0, graph)
+dist, node = get_max_dist(node, graph)
+print(dist+1)
