9955e29c96fdff24eeebdc1272bd0e435e3325f0
Otsuki Hitoshi <hitochan777@gmail.com>
2021-08-18 08:41:28 +0900

    Typical90 F

--- atcoder/typical90/F.py
+++ atcoder/typical90/F.py
@@ -1,19 +1,28 @@
 N, K = (int(x) for x in input().split())
 S = input()
 
-c = [[None] * 26 for _ in range(N)]
+c = [[K] * 26 for _ in range(N+1)]
+
+for i in range(26):
+  c[N][i] = N
 
 for i in range(N-1, -1, -1):
   for j in range(26):
-    c[i][j] = None
+    if S[i] == chr(ord('a') + j):
+      c[i][j] = i
+    else:
+      c[i][j] = c[i+1][j]
 
 ans = ""
+cur_pos = 0
 for i in range(K):
   for j in range(26):
-    next_pos = c[i][j]
+    next_pos = c[cur_pos][j]
     length = N - next_pos + i
     if length >= K:
       ans += chr(ord('a') + j)
+      cur_pos = next_pos + 1
+      break
 
 print(ans)
       
