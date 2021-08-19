af7e98880f64417cdbfff7e3190d1c587f3201b3
Otsuki Hitoshi <hitochan777@gmail.com>
2021-08-19 23:09:08 +0900

    Typical90 G

--- atcoder/typical90/G.py
+++ atcoder/typical90/G.py
@@ -1,4 +1,4 @@
-from bisect import bisect_left, bisect_right
+from bisect import bisect_left
 
 N = int(input())
 A = list(int(x) for x in input().split())
@@ -6,6 +6,5 @@ A.sort()
 Q = int(input())
 for _ in range(Q):
   B = int(input())
-  val1 = bisect_left(A, B)
-  val2 = bisect_right(A, B)
-  print(min(abs(val1 - B), abs(val2 - B)))
+  idx = bisect_left(A, B)
+  print(min(abs(A[idx-1] - B), abs(A[idx % N] - B)))
