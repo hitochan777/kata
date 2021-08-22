6915df4065af4d09cbfa9a4549017d85d0bf18d6
Otsuki Hitoshi <hitochan777@gmail.com>
2021-08-22 14:26:50 +0900

    WIP: ABC215 E

--- atcoder/abc215/E.py
+++ atcoder/abc215/E.py
@@ -1,24 +1,37 @@
 N = int(input())
 S = input()
 MOD = 998244353
-dp = [ [0] * 11 for _ in range(N+1)]
 
-# for c in range(10):
-#   dp[0][c] = 1
-dp[0][0] = 1
+def make_array(*args, default=int):
+  if len(args) == 0:
+    return default()
+
+  return [make_array(*args[1:]) for _ in range(args[0])]
+
+dp = make_array(N+1, 1<<10, 10)
 
 for i in range(N):
-  total = sum(dp[i]) % MOD
-  for c in range(11):
-    dp[i+1][c] = dp[i][c]
-    if c == 0:
-      continue
+  cur_c = ord(S[i]) - ord('A')
+  for s in range(1<<10):
+    for c in range(10):
+      dp[i+1][s][c] = dp[i][s][c]
+      if cur_c == c:
+        dp[i+1][s][c] += dp[i][s][c]
+        dp[i+1][s][c] %= MOD
 
-    if S[i] == chr(ord('A') + c - 1):
-      dp[i+1][c] += total
-      dp[i+1][c] %= MOD
+  for s in range(1<<10):
+    if (s >> cur_c) & 1 == 0:
+      for c in range(10):
+        dp[i+1][s | (1 << cur_c)][c] += dp[i][s][c]
+        dp[i+1][s | (1 << cur_c)][c] %= MOD
 
-    dp[i+1][c] %= MOD
+  dp[i+1][1 << cur_c][cur_c] += 1
+  dp[i+1][1 << cur_c][cur_c] %= MOD
 
-print(dp)
-print(sum(dp[N]))
+ans = 0
+for s in range(1<<10):
+  for c in range(10):
+    ans += dp[N][s][c]
+    ans %= MOD
+
+print(ans)
