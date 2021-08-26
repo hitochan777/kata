c936048c166eb89e22e8afd5110e85b3265c8146
Otsuki Hitoshi <hitochan777@gmail.com>
2021-08-26 20:51:46 +0900

    typical90 O

--- atcoder/typical90/O.py
+++ atcoder/typical90/O.py
@@ -22,12 +22,16 @@ class CombinationCalculator:
   def __init__(self, n: int, mod: int):
     self.mod = mod
     self.cache = [None] * (n + 1)
+    self._init_cache(n)
 
   def _init_cache(self, n: int) -> None:
     res = 1
-    for i in range(1, self.n + 1):
+    self.cache[0] = 1
+    for i in range(1, n + 1):
       res *= i
       res %= self.mod
+      self.cache[i] = res
+
 
   def factorial(self, n: int) -> int:
     return self.cache[n]
@@ -39,12 +43,13 @@ class CombinationCalculator:
     p = self.factorial(n)
     q = self.factorial(k)
     r = self.factorial(n-k)
-    return (p * pow_mod() *pow_mod(q, mod-2, mod)) % mod
+    return (p * pow_mod(q, self.mod - 2, self.mod) *pow_mod(r, self.mod-2, self.mod)) % self.mod
 
+cc = CombinationCalculator(N, MOD)
 for k in range(1, N + 1):
   ans = 0
   for a in range(1, math.ceil(N / k) + 1):
-    res = n_choose_k(N - (k - 1) * (a - 1), a, MOD)
+    res = cc.n_choose_k(N - (k - 1) * (a - 1), a)
     # print(N - (k - 1) * (a - 1), a, MOD, res)
     ans += res
     ans %= MOD
