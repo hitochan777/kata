59c714ba06e85b4163e0db158b4a79291d41248a
Otsuki Hitoshi <hitochan777@gmail.com>
2021-08-26 21:01:07 +0900

    refactor: typical90 O

--- atcoder/typical90/O.py
+++ atcoder/typical90/O.py
@@ -7,43 +7,34 @@ N = int(input())
 def div_mod(a: int, b: int, mod: int) -> int:
   return (a * inv_mod(b, mod)) % mod
 
-def pow_mod(a: int, n: int, mod: int) -> int:
-  res = 1
-  while n > 0:
-    if n & 1:
-      res = (res * a) % mod
-    
-    a = (a * a) % mod
-    n >>= 1
-  
-  return res
-
 class CombinationCalculator:
   def __init__(self, n: int, mod: int):
     self.mod = mod
     self.cache = [None] * (n + 1)
+    self.cache_inv = [None] * (n + 1)
     self._init_cache(n)
 
   def _init_cache(self, n: int) -> None:
     res = 1
     self.cache[0] = 1
+    self.cache_inv[0] = 1
     for i in range(1, n + 1):
       res *= i
       res %= self.mod
       self.cache[i] = res
-
+      self.cache_inv[i] = pow(res, self.mod - 2, self.mod)
 
   def factorial(self, n: int) -> int:
     return self.cache[n]
+
+  def factorial_inv(self, n: int) -> int:
+    return self.cache_inv[n]
 
   def n_choose_k(self, n: int, k: int) -> int:
     if n < k:
       return 0
 
-    p = self.factorial(n)
-    q = self.factorial(k)
-    r = self.factorial(n-k)
-    return (p * pow_mod(q, self.mod - 2, self.mod) *pow_mod(r, self.mod-2, self.mod)) % self.mod
+    return (self.factorial(n) * self.factorial_inv(k) * self.factorial_inv(n-k)) % self.mod
 
 cc = CombinationCalculator(N, MOD)
 for k in range(1, N + 1):
