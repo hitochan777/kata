3e931db9d637bbff5d2126945787646513d0b7e5
Otsuki Hitoshi <hitochan777@gmail.com>
2021-08-26 09:12:35 +0900

    wip: typical90 O

--- atcoder/typical90/O.py
+++ atcoder/typical90/O.py
@@ -1,27 +1,52 @@
 from atcoder.math import inv_mod
 import math
-MOD = 10**0 + 7
+MOD = 10**9 + 7
 
 N = int(input())
 
 def div_mod(a: int, b: int, mod: int) -> int:
   return (a * inv_mod(b, mod)) % mod
 
-def n_choose_k(n, k, mod):
-  if n < k:
-    return 0
+def pow_mod(a: int, n: int, mod: int) -> int:
+  res = 1
+  while n > 0:
+    if n & 1:
+      res = (res * a) % mod
+    
+    a = (a * a) % mod
+    n >>= 1
+  
+  return res
 
-  p, q = 1, 1
-  for i in range(k):
-    p = (p * (n - i)) % mod
-    q = (q * (i + 1)) % mod
+class CombinationCalculator:
+  def __init__(self, n: int, mod: int):
+    self.mod = mod
+    self.cache = [None] * (n + 1)
 
-  return (p * pow(q, mod-2, mod)) % mod
+  def _init_cache(self, n: int) -> None:
+    res = 1
+    for i in range(1, self.n + 1):
+      res *= i
+      res %= self.mod
+
+  def factorial(self, n: int) -> int:
+    return self.cache[n]
+
+  def n_choose_k(self, n: int, k: int) -> int:
+    if n < k:
+      return 0
+
+    p = self.factorial(n)
+    q = self.factorial(k)
+    r = self.factorial(n-k)
+    return (p * pow_mod() *pow_mod(q, mod-2, mod)) % mod
 
 for k in range(1, N + 1):
   ans = 0
   for a in range(1, math.ceil(N / k) + 1):
-    ans += n_choose_k(N - (k - 1) * (a - 1), a, MOD)
+    res = n_choose_k(N - (k - 1) * (a - 1), a, MOD)
+    # print(N - (k - 1) * (a - 1), a, MOD, res)
+    ans += res
     ans %= MOD
 
   print(ans)
