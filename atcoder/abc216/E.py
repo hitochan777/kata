b48bfb26e6990c1494abd533b79ab144e63c231c
Otsuki Hitoshi <hitochan777@gmail.com>
2021-08-29 23:19:46 +0900

    WIP: abc216 E

---
+++ atcoder/abc216/E.py
@@ -0,0 +1,29 @@
+from collections import Counter
+N, K = (int(x) for x in input().split())
+cnt = Counter(int(x) for x in input().split())
+A = [(int(k), v) for k, v in cnt.items()]
+A.sort(reverse=True)
+A.append((0, 0))
+cur = 0
+ans = 0
+
+def total(n):
+  return n * (n+1) // 2
+
+while K > 0 and cur < N:
+  val, cnt = A[cur]
+  nxt_val, nxt_cnt  = A[cur+1]
+   
+  if (val - nxt_val) * cnt <= K:
+    ans += (total(val) - total(nxt_val)) * cnt
+    K -= (val - nxt_val) * cnt
+    A[cur+1] = (nxt_val, nxt_cnt+cnt)
+  else:
+    tmp = (val - nxt_val) * cnt
+    d, m = divmod(K, cnt)
+    ans += (total(val) - total(val-d-1)) * d + (val-d+1) * m
+    K = 0
+
+  cur += 1
+
+print(ans)
