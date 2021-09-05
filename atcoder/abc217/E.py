1ed2f786170cf7cc842d3b38926bd83cbd0122c5
Otsuki Hitoshi <hitochan777@gmail.com>
2021-09-05 22:27:42 +0900

    abc217 E

---
+++ atcoder/abc217/E.py
@@ -0,0 +1,23 @@
+import heapq
+from collections import deque
+
+Q = int(input())
+q = deque()
+mq = []
+
+for _ in range(Q):
+  tokens = list(int(x) for x in input().split())
+  if tokens[0] == 1:
+    x = tokens[1]
+    q.append(x)
+  elif tokens[0] == 2:
+    if len(mq) == 0:
+      print(q.popleft())
+    else:
+      print(heapq.heappop(mq))
+  else:
+    while len(q) > 0:
+      val = q.pop()
+      heapq.heappush(mq, val)
+
+      # 5 5 3
