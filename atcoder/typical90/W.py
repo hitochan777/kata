345492b5956a12ea867e72990593b4f962bcf0e0
Otsuki Hitoshi <hitochan777@gmail.com>
2021-09-15 21:29:35 +0900

    WIP: Typical90 W

--- atcoder/typical90/W.py
+++ atcoder/typical90/W.py
@@ -17,15 +17,33 @@ for _ in range(H):
 dp = make_array(H+1, 1 << W)
 dp[0][0] = 1
 
+def is_ith_one(n, i):
+  ith = (n >> i) & 1
+  # print(i,ith)
+  return ith == 1
+
 for i in range(H):
   for j in range(1<<W):
     if dp[i][j] == 0:
       continue
 
     for k in range(1<<W):
-      if k == j or ((k - j) & j == 0 and only_one_bit(k - j)):
-        dp[i+1][k] += dp[i][j]
-        dp[i+1][k] %= MOD
+      valid = True
+      for l in range(W):
+        if (is_ith_one(j, l) and is_ith_one(k, l)) or (is_ith_one(j, l+1) and is_ith_one(k, l)) or (is_ith_one(j, l) and is_ith_one(k, l+1)):
+          valid = False
+          break
 
-print(dp)
+        if is_ith_one(k, l) and ((l-1 >= 0 and is_ith_one(k, l-1)) or (l+1 < W and is_ith_one(k, l+1))):
+          valid = False
+
+        if is_ith_one(j, l) and lines[i][l] == "#":
+          valid = False
+          break
+
+      if valid:
+          dp[i+1][k] += dp[i][j]
+          dp[i+1][k] %= MOD
+
+# print(dp)
 print(sum(dp[H]))
