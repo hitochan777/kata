02c232ab4224560333b42045973567c2bb6c0aa2
Otsuki Hitoshi <hitochan777@gmail.com>
2021-09-18 18:35:12 +0900

    WIP: Typical90 AB

---
+++ atcoder/typical90/AB.py
@@ -0,0 +1,30 @@
+N = int(input())
+
+def make_array(*args, default=int):
+  if len(args) == 0:
+    return default()
+
+  return [make_array(*args[1:], default=default) for _ in range(args[0])]
+
+L = 6
+paper = make_array(L,L)
+
+for _ in range(N):
+  lx, ly, rx, ry = (int(x) for x in input().split())
+  paper[lx][ly] += 1
+  paper[lx+1][ry+1] -= 1
+  paper[rx+1][ly+1] -= 1
+  paper[rx+1][ry+1] += 1
+
+for i in range(1,L):
+  for j in range(1,L):
+    paper[i][j] += paper[i][j-1]
+    paper[i][j] += paper[i-1][j]
+    
+areas = [0] * N
+for i in range(L):
+  for j in range(L):
+      areas[paper[i][j]-1] += 1
+
+for area in areas:
+  print(area)
