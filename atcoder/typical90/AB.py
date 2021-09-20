519c4d5579988bf735be8306bde7cb2228eea83c
Otsuki Hitoshi <hitochan777@gmail.com>
2021-09-20 23:21:43 +0900

    Typical90 AB

--- atcoder/typical90/AB.py
+++ atcoder/typical90/AB.py
@@ -6,24 +6,28 @@ def make_array(*args, default=int):
 
   return [make_array(*args[1:], default=default) for _ in range(args[0])]
 
-L = 6
+L = 1002
 paper = make_array(L,L)
 
 for _ in range(N):
   lx, ly, rx, ry = (int(x) for x in input().split())
   paper[lx][ly] += 1
-  paper[lx+1][ry+1] -= 1
-  paper[rx+1][ly+1] -= 1
-  paper[rx+1][ry+1] += 1
+  paper[lx][ry] -= 1
+  paper[rx][ly] -= 1
+  paper[rx][ry] += 1
 
-for i in range(1,L):
+for i in range(L):
   for j in range(1,L):
     paper[i][j] += paper[i][j-1]
+
+for j in range(L):
+  for i in range(1, L):
     paper[i][j] += paper[i-1][j]
-    
+
 areas = [0] * N
 for i in range(L):
   for j in range(L):
+    if paper[i][j] >= 1:
       areas[paper[i][j]-1] += 1
 
 for area in areas:
