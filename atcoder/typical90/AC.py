0ca28db93102437d9d5846c8935eae1cb633e32f
Otsuki Hitoshi <hitochan777@gmail.com>
2021-09-23 14:56:02 +0900

    Typical90 AC

--- atcoder/typical90/AC.py
+++ atcoder/typical90/AC.py
@@ -4,10 +4,12 @@ W, N = (int(x) for x in input().split())
 op = lambda a, b: max(a, b)
 e = -10**18
 id_ = 0 
-mapping = lambda a, b: a + b 
-composition = lambda a, b: a + b
-lst = LazySegTree(op=op, e=e, mapping=mapping, composition=composition, id_=id_, v=[0] * (5 * 10**5 + 1))
+mapping = lambda a, b: b if a == id_ else a
+composition = lambda a, b: b if a == id_ else a
+lst = LazySegTree(op=op, e=e, mapping=mapping, composition=composition, id_=id_, v=[0] * (W + 1))
 for _ in range(N):
   L, R = (int(x) for x in input().split())
-  lst.apply(L, R+1, 1)
-  print(lst.prod(L, R+1))
+  L, R = L-1, R-1
+  mx = lst.prod(L, R+1)
+  lst.apply(L, R+1, mx+1)
+  print(mx+1)
