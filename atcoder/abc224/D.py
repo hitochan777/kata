d8b2bedb90b6f7b3f865e321bd2eedb21dcdfd6c
Otsuki Hitoshi <hitochan777@gmail.com>
2021-10-24 01:08:04 +0900

    ABC224 D: dict version

--- atcoder/abc224/D.py
+++ atcoder/abc224/D.py
@@ -10,15 +10,30 @@ for _ in range(M):
 p = list(int(x)-1 for x in input().split())
 visited = set()
 
+def is_goal(p):
+  return all(key == val for key, val in p.items() if key != 8)
+
 def dict_hash(dictionary):
-  s = sum((10**i) * dictionary[i] for i in range(9))
-  return s
+  return sum(10**i * dictionary[i] for i in dictionary)
+
+def gen_next_states(g, p, visited, empty_idx):
+  ret = []
+  for nb in g[empty_idx]:
+    p2 = dict(p)
+    p2[empty_idx], p2[nb] = p2[nb], p2[empty_idx]
+    hash = dict_hash(p2)
+    if hash in visited:
+      continue
+
+    ret.append((p2, nb, hash))
+  
+  return ret
 
 p_dict = {el: i for i, el in enumerate(p)}
 empty_idx = None
 for i in range(9):
   if i not in p_dict:
-    p_dict[i] = 8
+    p_dict[i] = -1
     empty_idx = i
     break
 
@@ -29,18 +44,12 @@ visited.add(dict_hash(p_dict))
 ans = -1
 while len(q) > 0:
   state, empty_idx, cnt, hash = q.popleft()
-  if all(key == val for key, val in state.items() if key != 8):
+  if is_goal(state):
     ans = cnt
     break
 
-  for nb in g[empty_idx]:
-    p2 = dict(state)
-    p2[empty_idx], p2[nb] = p2[nb], p2[empty_idx]
-    hash = dict_hash(p2)
-    if hash in visited:
-      continue
-
-    q.append((p2, nb, cnt+1, hash))
+  for nxt, empty_idx, hash in gen_next_states(g, state, visited, empty_idx):
+    q.append((nxt, empty_idx, cnt+1, hash))
     visited.add(hash)
 
 print(ans)
