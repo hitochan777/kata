b3bb330e8176f91800915db6fe7539585672600d
Otsuki Hitoshi <hitochan777@gmail.com>
2021-10-24 16:42:50 +0900

    typical90 AK

--- atcoder/typical90/AK.py
+++ atcoder/typical90/AK.py
@@ -5,7 +5,7 @@ def make_array(*args, default=int):
   if len(args) == 0:
     return default()
 
-  return [make_array(*args[1:]) for _ in range(args[0])]
+  return [make_array(*args[1:], default=default) for _ in range(args[0])]
 
 dishes = []
 for _ in range(N):
@@ -13,19 +13,23 @@ for _ in range(N):
   dishes.append((L, R, V))
   
 MAX_SPICE = W + 1
-dp = make_array(N+1, MAX_SPICE)
-
+dp = make_array(N+1, MAX_SPICE, default=lambda: -10**18)
+dp[0][0] = 0
 for i in range(N):
   l, r, v = dishes[i]
-  st = SegTree(lambda a, b: max(a,b), 0, [dp[i][j] for j in range(MAX_SPICE)])
   for j in range(MAX_SPICE):
     dp[i+1][j] = dp[i][j]
-    l, r = max(0, j-l), max(0, j-r+1)
-    if l > r:
+
+  st = SegTree(lambda a, b: max(a,b), -10**18, [dp[i][j] for j in range(MAX_SPICE)])
+  for j in range(MAX_SPICE):
+    cl, cr = max(0, j-r), max(0, j-l+1)
+    if cl == cr:
       continue
 
-    mx = st.prod(l, r) + v
-    dp[i+1][j] = max(dp[i+1][j], mx)
+    mx = st.prod(cl, cr)
+    if mx == -10**18:
+      continue
 
-# print(dp)
-print(dp[N][W] if dp[N][W] > 0 else -1)
+    dp[i+1][j] = max(dp[i+1][j], mx + v)
+
+print(dp[N][W] if dp[N][W] != -10**18 else -1)
