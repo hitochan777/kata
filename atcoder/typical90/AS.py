addeef265f81ceb01b6fbb6112ef91d62e1bae71
Otsuki Hitoshi <hitochan777@gmail.com>
2021-10-27 22:44:15 +0900

    typical90 AS

--- atcoder/typical90/AS.py
+++ atcoder/typical90/AS.py
@@ -10,7 +10,7 @@ for _ in range(N):
   X, Y = (int(x) for x in input().split())
   C.append((X,Y))
 
-dp = make_array(2**N, K+1)
+dp = make_array(K+1, 2**N, default=lambda: 10**18)
 d = make_array(2**N)
 
 for i in range(1<<N):
@@ -27,13 +27,15 @@ for i in range(1<<N):
 
   d[i] = mx
 
+dp[0][0] = 0
+
 for i in range(1<<N):
   for j in range(1,K+1):
     k = i
     while k != 0:
-      mx = max(dp[k][j-1], d[i-k])
-      dp[i][j] = min(dp[i][j], mx)
+      mx = max(dp[j-1][i-k], d[k])
+      dp[j][i] = min(dp[j][i], mx)
       k = (k-1) & i
 
-print(dp[1<<N-1][K])
+print(dp[K][(1<<N)-1])
 
