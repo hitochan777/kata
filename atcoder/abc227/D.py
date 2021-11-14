53ffec98024e90f46c05e28a4300e368b925fe32
Otsuki Hitoshi <hitochan777@gmail.com>
2021-11-14 10:38:34 +0900

    ABC227 D

--- atcoder/abc227/D.py
+++ atcoder/abc227/D.py
@@ -1,17 +1,14 @@
 N, K = (int(x) for x in input().split())
-cnt = [0] * (N + 2)
 A = list(int(x) for x in input().split())
-for a in A:
-  cnt[1] += 1
-  cnt[min(N, a)+1] += -1
+l, h = 0, 10**18
+while h - l > 1:
+  m = (l+h) >> 1
+  total = sum(min(a, m) for a in A)
+  if total >= K * m:
+    l = m
+  else:
+    h = m
 
-total = 0
-rem = 0
-for i in range(1,N+2):
-  cnt[i] += cnt[i-1]
-  total += (cnt[i] + rem) // K
-  if (cnt[i] + rem) // K > 0:
-    rem = (cnt[i] + rem) % K
+print(l)
 
-print(total)
 
