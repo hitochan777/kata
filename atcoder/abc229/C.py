1e7198983a7402e9c0596569951c414207e7f8eb
Otsuki Hitoshi <hitochan777@gmail.com>
2021-11-29 00:03:20 +0900

    abc229 C

--- atcoder/abc229/C.py
+++ atcoder/abc229/C.py
@@ -2,16 +2,16 @@ N, W = (int(x) for x in input().split())
 nums = []
 for _ in range(N):
   a, b = (int(x) for x in input().split())
-  nums.append((b, a))
+  nums.append((a, b))
 
 nums.sort(reverse=True)
 total = 0
-for b, a in nums:
-  diff = min(W, a)
+i = 0
+while W > 0 and i < N:
+  a, b = nums[i]
+  diff = min(W, b)
   W -= diff
-  if W == 0:
-    break
-
-  total += diff * b
+  total += diff * a
+  i += 1
 
 print(total)
