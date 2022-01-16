f4c1287840a708009f5e794ff7f33fc77ed9aec9
Otsuki Hitoshi <hitochan777@gmail.com>
2022-01-16 16:47:11 +0900

    ABC235 E

--- atcoder/abc235/E.py
+++ atcoder/abc235/E.py
@@ -1,69 +1,23 @@
-from bisect import insort
-
-class Graph:
-    def __init__(self, vertices):
-        self.V = vertices
-        self.graph = []
- 
-    def addEdge(self, u, v, w):
-        self.graph.append((w, u, v))
-
-    def find(self, parent, i):
-        if parent[i] == i:
-            return i
-        return self.find(parent, parent[i])
- 
-    def union(self, parent, rank, x, y):
-        xroot = self.find(parent, x)
-        yroot = self.find(parent, y)
-        if rank[xroot] < rank[yroot]:
-            parent[xroot] = yroot
-        elif rank[xroot] > rank[yroot]:
-            parent[yroot] = xroot
-        else:
-            parent[yroot] = xroot
-            rank[xroot] += 1
- 
-    def fixGraph(self):
-      self.graph = sorted(self.graph)
-
-    def KruskalMST(self, a, b, c):
-        i = 0
-        e = 0
-        graph = self.graph[:] 
-        insort(graph, (c, a, b))
-        parent = []
-        rank = []
-        for node in range(self.V):
-            parent.append(node)
-            rank.append(0)
- 
-        while e < self.V - 1:
-            w, u, v = graph[i]
-            i = i + 1
-            x = self.find(parent, u)
-            y = self.find(parent, v)
- 
-            if x != y:
-                e = e + 1
-                if w == c:
-                  print("Yes")
-                  return
-
-                self.union(parent, rank, x, y)
-
-        print("No")
+from atcoder.dsu import DSU
 
 N, M, Q = (int(x) for x in input().split())
-g = Graph(N)
-for _ in range(M):
+edges = []
+for i in range(M+Q):
   a, b, c = (int(x) for x in input().split()) 
   a, b = a-1, b-1
-  g.addEdge(a, b, c)
+  edges.append((c, a, b, i))
 
-g.fixGraph()
+dsu = DSU(N)
+edges = sorted(edges)
+result = [""] * Q
+for w, u, v, l in edges:
+  if l >= M:
+    result[l-M] = not dsu.same(u, v)
+  else: 
+    dsu.merge(u, v)
 
-for _ in range(Q):
-  u, v, w = (int(x) for x in input().split())
-  u, v = u-1, v-1
-  g.KruskalMST(u, v, w)
+for ok in result:
+  if ok:
+    print("Yes")
+  else:
+    print("No")
