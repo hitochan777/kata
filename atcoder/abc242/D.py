b1221017cefb55d34bd90cc3a7d2b4f278094b82
Otsuki Hitoshi <hitochan777@gmail.com>
2022-03-06 00:11:08 +0900

    abc242 D

--- atcoder/abc242/D.py
+++ atcoder/abc242/D.py
@@ -1,25 +1,16 @@
-import math
-def solve(S: str, t: int, k: int):
-  if t >= 60:
-    target = 0
-    offset = k - 1
-  else:
-    target = (k-1) // (2^t)
-    offset = (k-1) % (2^t)
+def solve(S, t, k):
+  if t == 0:
+    return S[k]
+  
+  if k == 0:
+    offset = ((ord(S[0]) - ord("A")) + t) % 3
+    return chr(ord("A") + offset)
 
-  s0 = (ord(S[target]) - ord("a") + (t % 3)) % 3
-  ancestor = (s0 - int(math.log2(offset)) + 300000) % 3
-  s = "ABC"
-  for c in bin(offset)[2:]:
-    if c == "1":
-      ancestor = (ancestor + 1) % 3
-
-  return s[ancestor]
-
+  return chr(ord("A") + ((ord(solve(S, t-1, k//2)) - ord("A") + k % 2 + 1) % 3))
 
 S = input()
 Q = int(input())
 for _ in range(Q):
   t, k = (int(x) for x in input().split())
-  ans =  solve(S, t, k)
+  ans =  solve(S, t, k-1)
   print(ans)
