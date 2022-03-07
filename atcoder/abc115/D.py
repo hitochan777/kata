c1695dae97ab6a12269fcedce1dd323a318916ee
Otsuki Hitoshi <hitochan777@gmail.com>
2022-03-08 00:13:30 +0900

    abc115 D

--- atcoder/abc115/D.py
+++ atcoder/abc115/D.py
@@ -4,37 +4,33 @@ def f(l, x):
   if l == 0:
     return 1
 
-  if x == 0:
+  if x < 1:
     return 0
 
-  s = 0
   x -= 1
   if x < total[l-1]:
-    s += f(l-1, x)
-    return s
+    return f(l-1, x)
 
   x -= total[l-1]
   if x < 1:
-    s += 1
-    return s
+    return patty[l-1] + 1
 
   x -= 1
   if x < total[l-1]:
-    s += f(l-1, x)
-    return s
+    return patty[l-1] + 1 + f(l-1, x)
 
-  s += patty[l-1]
-  return s
+
+  return patty[l-1] * 2 + 1 
 
 
 total = [0] * (N+1)
-total[0] = 0
+total[0] = 1
 for i in range(N):
   total[i+1] = total[i] * 2 + 3
 
 patty = [0] * (N+1)
-patty[1] = 1
-for i in range(1,N):
+patty[0] = 1
+for i in range(N):
   patty[i+1] = patty[i] * 2 + 1
-
+  
 print(f(N,X-1))
