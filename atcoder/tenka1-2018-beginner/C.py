262a45bbd1b8fd7d887bb40c6e7f0dd499d4fd09
Otsuki Hitoshi <hitochan777@gmail.com>
2022-03-12 16:43:58 +0900

    code-festival-2014-final C

--- atcoder/tenka1-2018-beginner/C.py
+++ atcoder/tenka1-2018-beginner/C.py
@@ -1,4 +1,3 @@
-from collections import deque
 N = int(input())
 li = []
 for _ in range(N):
@@ -6,30 +5,54 @@ for _ in range(N):
   li.append(A)
 
 li.sort()
-queue = deque()
-p = [0, N-1]
-queue.append(li[0])
-for i in range(1,N):
-  if (i // 2) % 2 == 0:
-    queue.append(li[p[i%2]])
-  else:
-    queue.appendleft(li[p[i%2]])
-
-  if i % 2 == 0:
-    p[i%2] += 1
-  else:
-    p[i%2] -= 1
 
 total = 0
-prev = None
-while len(queue) > 0:
-  val = queue.pop()
-  print(val)
-  if prev != None:
-    total += abs(prev - val)
+if N % 2 == 0:
+  i = 0
+  for _ in range((N - 2) // 2):
+    total += -2 * li[i]
+    i += 1
 
-  prev = val
+  total += -li[i]
+  i += 1
+  total += li[i]
+  i += 1
+  for _ in range((N-2)//2):
+    total += 2 * li[i]
+    i += 1
+
+else:
+  total1 = 0
+  i = 0
+  for _ in range((N-2)//2+1):
+    total1 += -2 * li[i]
+    i += 1
+
+  total1 += li[i]
+  i += 1
+  total1 += li[i]
+  i += 1
+  for _ in range((N-2)//2):
+    total1 += 2 * li[i]
+    i += 1
+
+  total2 = 0
+  i = 0
+  for _ in range((N-2)//2):
+    total2 += -2 * li[i]
+    i += 1
+
+  total2 -= li[i]
+  i += 1
+  total2 -= li[i]
+  i += 1
+  for _ in range((N-2)//2+1):
+    total2 += 2 * li[i]
+    i += 1 
+
+  total = max(total1, total2)
 
 print(total)
+
 
 
