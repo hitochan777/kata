6ce0c70601454b9c38ae106e567c4ef3d21d18b9
Otsuki Hitoshi <hitochan777@gmail.com>
2022-03-20 23:36:07 +0900

    ABC244 E

--- atcoder/abc244/E.py
+++ atcoder/abc244/E.py
@@ -1,6 +1,9 @@
 from collections import defaultdict
 MOD = 998244353
 N, M, K, S, T, X = (int(x) for x in input().split())
+S -= 1
+T -= 1
+X -= 1
 g = defaultdict(list)
 for _ in range(M):
   U, V = (int(x) for x in input().split())
@@ -8,12 +11,23 @@ for _ in range(M):
   g[U].append(V)
   g[V].append(U)
 
-prev = defaultdict(int)
-for i in range(K):
-  d = defaultdict(int)
-  for i in range(N):
-    for nb in g[i]:
-      d[i] += prev[i]
-      d[i] %= MOD
+  def make_array(*args):
+    if len(args) == 0:
+      return 0 
+  
+    return [make_array(*args[1:]) for _ in range(args[0])]
 
-  prev = d
+dp = make_array(K+1,N,2)
+dp[0][S][0] = 1
+
+for i in range(K):
+  for j in range(N):
+    for x in range(2):
+      if j == X:
+        dp[i+1][X][1-x] += sum(dp[i][k][x] for k in g[X])
+        dp[i+1][X][1-x] %= MOD
+      else:
+        dp[i+1][j][x] += sum(dp[i][k][x] for k in g[j])
+        dp[i+1][j][x] %= MOD
+
+print(dp[K][T][0])
