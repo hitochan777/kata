e067bbceea03b43e1f787da36ca4773707fae4ce
Otsuki Hitoshi <hitochan777@gmail.com>
2022-03-20 23:56:37 +0900

    ABC244 E

--- atcoder/abc244/E.py
+++ atcoder/abc244/E.py
@@ -1,33 +1,29 @@
-from collections import defaultdict
 MOD = 998244353
 N, M, K, S, T, X = (int(x) for x in input().split())
 S -= 1
 T -= 1
 X -= 1
-g = defaultdict(list)
+edges = []
 for _ in range(M):
   U, V = (int(x) for x in input().split())
   U, V = U-1, V-1
-  g[U].append(V)
-  g[V].append(U)
+  edges.append((U,V))
 
-  def make_array(*args):
-    if len(args) == 0:
-      return 0 
-  
-    return [make_array(*args[1:]) for _ in range(args[0])]
+def make_array(*args):
+  if len(args) == 0:
+    return 0 
 
-dp = make_array(K+1,N,2)
-dp[0][S][0] = 1
+  return [make_array(*args[1:]) for _ in range(args[0])]
+
+dp = make_array(2,K+1,N)
+dp[0][0][S] = 1
 
 for i in range(K):
-  for j in range(N):
+  for u, v in edges:
     for x in range(2):
-      if j == X:
-        dp[i+1][X][1-x] += sum(dp[i][k][x] for k in g[X])
-        dp[i+1][X][1-x] %= MOD
-      else:
-        dp[i+1][j][x] += sum(dp[i][k][x] for k in g[j])
-        dp[i+1][j][x] %= MOD
+      dp[x][i+1][v] += dp[x ^ (v == X)][i][u]
+      dp[x][i+1][v] %= MOD
+      dp[x][i+1][u] += dp[x ^ (u == X)][i][v]
+      dp[x][i+1][u] %= MOD
 
-print(dp[K][T][0])
+print(dp[0][K][T])
