28bd256177f7cc6887cba84a665c6dd89a7fa4e7
Otsuki Hitoshi <hitochan777@gmail.com>
2022-03-21 15:46:53 +0900

    ABC244 E: another solution

--- atcoder/abc244/E.py
+++ atcoder/abc244/E.py
@@ -1,29 +1,30 @@
+from collections import defaultdict
 MOD = 998244353
 N, M, K, S, T, X = (int(x) for x in input().split())
 S -= 1
 T -= 1
 X -= 1
-edges = []
+g = defaultdict(list)
 for _ in range(M):
   U, V = (int(x) for x in input().split())
   U, V = U-1, V-1
-  edges.append((U,V))
+  g[U].append(V)
+  g[V].append(U)
 
-def make_array(*args):
-  if len(args) == 0:
-    return 0 
+  def make_array(*args):
+    if len(args) == 0:
+      return 0 
+  
+    return [make_array(*args[1:]) for _ in range(args[0])]
 
-  return [make_array(*args[1:]) for _ in range(args[0])]
-
-dp = make_array(2,K+1,N)
-dp[0][0][S] = 1
+dp = make_array(K+1,N,2)
+dp[0][S][0] = 1
 
 for i in range(K):
-  for u, v in edges:
+  for j in range(N):
     for x in range(2):
-      dp[x][i+1][v] += dp[x ^ (v == X)][i][u]
-      dp[x][i+1][v] %= MOD
-      dp[x][i+1][u] += dp[x ^ (u == X)][i][v]
-      dp[x][i+1][u] %= MOD
+      for k in g[j]:
+        dp[i+1][j][x] += dp[i][k][x ^ (j==X)]
+        dp[i+1][j][x] %= MOD
 
-print(dp[0][K][T])
+print(dp[K][T][0])
