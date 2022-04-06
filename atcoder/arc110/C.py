9e055f2af0ae40835d8b1424d227fcc089711592
Otsuki Hitoshi <hitochan777@gmail.com>
2022-04-06 17:04:52 +0900

    arc110 C

--- atcoder/arc110/C.py
+++ atcoder/arc110/C.py
@@ -2,22 +2,22 @@ N = int(input())
 P = list(int(x) for x in input().split())
 
 target = 1
-p = 0
 ops = []
-while p < N - 1:
-  idx = P.index(target)
-  i = idx
-  for i in range(idx, max(target - 1, p), -1):
-    P[i], P[i-1] = P[i-1], P[i]
-    ops.append(i)
-    i -= 1
+for i, v in enumerate(P):
+  if v == target:
+    for j in range(i, target-1, -1):
+      P[j], P[j-1] = P[j-1], P[j]
+      ops.append(j)
 
-  if not all(P[i] == i+1 for i in range(p, idx)):
-    print("-1")
-    exit()
+    target = i + 1
 
-  p = idx
-  target = idx+1
+if not all(i == v for i, v in enumerate(P, start=1)):
+  print("-1")
+  exit()
+
+if len(ops) != N-1:
+  print(-1)
+  exit()
 
 for op in ops:
   print(op)
