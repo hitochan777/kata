7528743e46e5aba632e8643e5a08a211c024a622
Otsuki Hitoshi <hitochan777@gmail.com>
2022-04-14 23:51:07 +0900

    abc247 E

--- atcoder/abc247/E.py
+++ atcoder/abc247/E.py
@@ -2,7 +2,18 @@ N, X, Y = (int(x) for x in input().split())
 A = list(int(x) for x in input().split())
 
 def solve(A):
-  n = len(A)
-  m = n >> 1
-  x1, x2 = solve(A[:m])
-  y1, y2 = solve(A[m])
+  cnt = 0
+  px, py, pz = -1, -1, -1
+  for i, a in enumerate(A):
+    if a == X:
+      px = i
+    if a == Y:
+      py = i
+    if a < Y or a > X:
+      pz = i 
+
+    cnt += max(0, min(px, py) - pz)
+
+  return cnt
+
+print(solve(A))
