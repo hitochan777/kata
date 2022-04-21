11f0500e291e1de149a32e06ccf227efa0381d6b
Otsuki Hitoshi <hitochan777@gmail.com>
2022-04-21 22:51:52 +0900

    arc121 B

--- atcoder/arc121/B.py
+++ atcoder/arc121/B.py
@@ -1,5 +1,16 @@
 from bisect import bisect_left
 
+def find_min(a, b):
+  ans = 10**18
+  for n in a:
+    idx = bisect_left(b, n)
+    if idx < len(b):
+      ans = min(ans, abs(n - b[idx]))
+
+    if idx > 0:
+      ans = min(ans, abs(n - b[idx-1]))
+  
+  return ans
 
 N = int(input())
 cnt = {"R": [], "G": [], "B": []}
@@ -7,24 +18,19 @@ for _ in range(N * 2):
   a, c = input().split()
   cnt[c].append(int(a))
 
-cnts = []
+odds, evens = [], []
 for v in cnt.values():
-  if len(v) % 2 != 0:
-    cnts.append(v)
+  if len(v) % 2 == 0:
+    evens.append(v)
+  else:
+    odds.append(v)
 
-if len(cnts) == 0:
+if len(odds) == 0:
   print(0)
   exit()
 
-assert len(cnts) == 2, len(cnts)
-cnts[1].sort()
-ans = 10**18
-for n in cnts[0]:
-  idx = bisect_left(cnts[1], n)
-  if idx < len(cnts[1]):
-    ans = min(ans, abs(n - cnts[1][idx]))
-
-  if idx > 0:
-    ans = min(ans, abs(n - cnts[1][idx-1]))
-
+odds[1].sort()
+evens[0].sort()
+ans = find_min(odds[0], odds[1])
+ans = min(ans, find_min(odds[0], evens[0]) + find_min(odds[1], evens[0]))
 print(ans)
