8f13cf2b5d2d591e6a39956bcf0109a501501f51
Otsuki Hitoshi <hitochan777@gmail.com>
2022-05-10 23:33:43 +0900

    abc125 C

--- atcoder/abc125/C.py
+++ atcoder/abc125/C.py
@@ -1,25 +1,25 @@
-from functools import reduce
-
-def gcd(a,b): 
-    if(b==0): 
-        return a 
-    else: 
-        return gcd(b,a%b) 
+from math import gcd
 
 n = int(input())
 A = list(map(int, input().split()))
+
+L = [0] * n
+R = [0] * n
+L[0], R[-1] = A[0], A[-1]
+
+for i in range(1, n):
+    L[i] = gcd(L[i-1], A[i])
+
+L = [0] + L
+
+for i in range(n-2, -1, -1):
+    R[i] = gcd(R[i+1], A[i])
+
+R = R + [0]
+
 maxGcd = 0
-
-L = [0] * (n + 2)
-R = [0] * (n + 2)
-
-for i in range(2, n+2):
-    L[i] = gcd(L[i-1], A[i-2])
-
-for i in range(n, 0, -1):
-    R[i] = gcd(R[i+1], A[i-1])
-
-for i in range(1, n+1):
-    maxGcd = max(maxGcd, gcd(L[i-1], R[i]))
+for i in range(n):
+    g = gcd(L[i] if L[i] != 0 else R[i+1], R[i+1] if R[i+1] != 0 else L[i])
+    maxGcd = max(maxGcd, g)
 
 print(maxGcd)
