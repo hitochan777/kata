18ea5fb95d322f40d511382e82f538ed08ecdaaf
Otsuki Hitoshi <hitochan777@gmail.com>
2022-05-31 00:15:24 +0900

    abc128 C

--- atcoder/abc128/C.py
+++ atcoder/abc128/C.py
@@ -2,11 +2,21 @@ N, M = (int(x) for x in input().split())
 
 lights = []
 for _ in range(M):
-  k, *s = list(int(x) for x in input().split())
+  k, *s = list(int(x)-1 for x in input().split())
   lights.append(s)
 
 p = list(int(x) for x in input().split())
 
+ans = 0
 for i in range(1<<N):
-  for j in range(N)
-    (i >> j) & 1
+  on_nums = set(j for j in range(N) if (i >> j) & 1 == 1)
+  total = 0
+  for j, light in enumerate(lights):
+    cnt = sum(1 for switch in light if switch in on_nums)
+    if cnt % 2 == p[j]:
+      total += 1
+    
+  if total == M:
+    ans += 1
+
+print(ans)
