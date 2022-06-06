96d2bbddb9d9bcde8753f99a1b35b74a28a08960
Otsuki Hitoshi <hitochan777@gmail.com>
2022-06-07 00:57:34 +0900

    abc254 D

--- atcoder/abc254/D.py
+++ atcoder/abc254/D.py
@@ -1,11 +1,25 @@
+from collections import defaultdict
+
 N = int(input())
 
-ss = set()
-i = 1
-while i**2 <= N**2:
-  ss.add(i**2)
-  i += 1
+square_nums = set()
+for i in range(1,N+1):
+  square_nums.add(i**2)
 
-print(len(ss))
-# while i**2 <= N**2:
-#   if i 
+divisors = defaultdict(list)
+for i in range(1, N+1):
+  for j in range(i,N+1,i):
+    divisors[j].append(i)
+
+cnt = defaultdict(int)
+for i in range(1, N+1):
+  for j in range(len(divisors[i])-1, -1, -1):
+    if divisors[i][j] in square_nums:
+      cnt[i/divisors[i][j]] += 1
+      break
+
+ans = 0
+for i in range(1, N+1):
+  ans += cnt[i]**2
+
+print(ans)
