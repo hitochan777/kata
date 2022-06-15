6de62f3c6e5c6e5d9c9c663a1839974cdd5e6e01
Otsuki Hitoshi <hitochan777@gmail.com>
2022-06-15 22:26:33 +0900

    arc124 B

--- atcoder/arc124/B.py
+++ atcoder/arc124/B.py
@@ -2,14 +2,17 @@ N = int(input())
 A = list(int(x) for x in input().split())
 A.sort()
 B = list(int(x) for x in input().split())
-ans = []
-for i in A:
-  x = B[0]^i
+ans = set() 
+
+for a in A:
+  x = a^B[0]
   C = list(map(lambda b: b^x, B))
   C.sort()
   if all(a == c for a, c in zip(A, C)):
-    ans.append(x)
+    ans.add(x)
 
-print(len(ans))
-for val in ans:
+ans_li = list(ans)
+ans_li.sort()
+print(len(ans_li))
+for val in ans_li:
   print(val)
