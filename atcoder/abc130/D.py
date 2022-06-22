e12e0e78e3cc440619129013f3ef77e4330dfcab
Otsuki Hitoshi <hitochan777@gmail.com>
2022-06-22 23:13:50 +0900

    Update

--- atcoder/abc130/D.py
+++ atcoder/abc130/D.py
@@ -1,19 +1,22 @@
 N, K = (int(x) for x in input().split())
 A = list(int(x) for x in input().split())
 
+ans = 0
 res = N+1
-right = 0;
-total = 0;
+right = 0
+total = 0
 for left in range(N):
-  while right < N and total < x:
-    sum += A[right]
+  while right < N and total < K:
+    total += A[right]
     right += 1
 
   if total < K:
     break
   
-  res = min(res, right - left)
+  ans += N - (right - 1)
   if right == left:
     right += 1
   else:
     total -= A[left]
+
+print(ans)
