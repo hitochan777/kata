290fd1f4d7cca7e2b90b959e725794de30706168
Otsuki Hitoshi <hitochan777@gmail.com>
2022-06-28 21:45:15 +0900

    abc200 D

---
+++ atcoder/abc200/D.py
@@ -0,0 +1,29 @@
+from collections import defaultdict
+
+N = int(input())
+A = list(int(x) for x in input().split())
+n = min(N, 8)
+
+lists = defaultdict(list)
+
+for i in range(1<<n):
+  total = 0
+  seq = []
+  for j in range(n):
+    if (i >> j) & 1 == 1:
+      seq.append(j+1)
+      total += A[j]
+      total %= 200
+       
+  if len(lists[total]) > 0:
+    print("Yes")
+    print(len(lists[total][0]), *lists[total][0])
+    print(len(seq), *seq)
+    exit()
+  else:
+    lists[total].append(seq)
+    
+print("No")
+  
+  
+      
