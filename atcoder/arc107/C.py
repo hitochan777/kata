64e410af239917e1b7babd52634e5aa6b3c280e3
Otsuki Hitoshi <hitochan777@gmail.com>
2022-07-02 15:33:19 +0900

    arc107 C

--- atcoder/arc107/C.py
+++ atcoder/arc107/C.py
@@ -1,5 +1,8 @@
 import sys
+from atcoder.scc import SCCGraph
+
 sys.setrecursionlimit(10**5)
+MOD = 998244353
 
 def factorial(n, mod):
   if n == 1:
@@ -8,26 +11,35 @@ def factorial(n, mod):
   val = n * factorial(n-1, mod)
   return val % mod
 
-  
+
 N, K = (int(x) for x in input().split())
 A = []
 for _ in range(N):
   A.append(list(int(x) for x in input().split()))
 
-
+ans = 1
+scc = SCCGraph(N)
 rcnt = 0
 for i in range(N):
   for j in range(i+1, N):
     if all(A[i][k] + A[j][k] <= K for k in range(N)):
-      rcnt += 1
+      scc.add_edge(i, j)
+      scc.add_edge(j, i)
+      
+for nodes in scc.scc():
+  ans *= factorial(len(nodes), MOD)
+  ans %= MOD
       
 ccnt = 0
+scc = SCCGraph(N)
 for i in range(N):
   for j in range(i+1, N):
     if all(A[k][i] + A[k][j] <= K for k in range(N)):
-      ccnt += 1
+      scc.add_edge(i, j)
+      scc.add_edge(j, i)
       
-MOD = 998244353
-print(rcnt+1, ccnt+1)
-ans = (factorial(rcnt+1, MOD) * factorial(ccnt+1, MOD)) % MOD
+for nodes in scc.scc():
+  ans *= factorial(len(nodes), MOD)
+  ans %= MOD
+      
 print(ans)
