d051a447ee0fc31400a5e133b6a140949089f9c0
Otsuki Hitoshi <hitochan777@gmail.com>
2022-07-04 23:51:14 +0900

    abc258 E

--- atcoder/abc258/E.py
+++ atcoder/abc258/E.py
@@ -6,38 +6,37 @@ W = list(int(x) for x in input().split())
 idx = defaultdict(int)
 base_cnt = 0
 sw = sum(W)
-if sw >= X:
-  base_cnt = (X // sw) * N
+if sw <= X:
+  base_cnt = (X // sw) * N 
   X %= sw
   
 acc = [0]
-for w in W:
+for w in (W + W):
   acc.append(acc[-1] + w)
 
 start_idx = 0
 start_idx_set = set()
 start_idx_set.add(0)
+# print(acc, X)
 C = []
+m = {}
 while True:
-  target = X + acc[start_idx]
-  idx = bisect_left(acc, target)
-  if idx >= N+1:
-    idx = bisect_left(acc, X - (acc[-1] - acc[start_idx]))
-    
-  C.append((N + idx - start_idx) % N)
-  C.append(N if idx == start_idx else idx - start_idx)
-  if idx in start_idx_set:
-    repeat_start_idx = idx
+  if start_idx in m:
+    repeat_start_idx = m[start_idx]
     break
 
-  start_idx = idx
-  start_idx_set.add(idx)
+  m[start_idx] = len(C)
+  target = X + acc[start_idx]
+  idx = bisect_left(acc, target)
+  C.append(idx - start_idx)
+  # print(C, idx, target)
+  start_idx = idx % N
   
-print(repeat_start_idx, C)
+# print(repeat_start_idx, C)
 for _ in range(Q):
   K = int(input()) - 1
   if K <= repeat_start_idx:
     print(C[K] + base_cnt)
   else:
-    i = (K - repeat_start_idx) % (len(C) - repeat_start_idx - 1) + repeat_start_idx
+    i = (K - repeat_start_idx) % (len(C) - repeat_start_idx) + repeat_start_idx
     print(C[i] + base_cnt)
