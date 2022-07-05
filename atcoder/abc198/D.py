e6cd1b3a4db18d6e5035d17bd1fcf6cd0f73444b
Otsuki Hitoshi <hitochan777@gmail.com>
2022-07-05 22:24:48 +0900

    abc198 D

--- atcoder/abc198/D.py
+++ atcoder/abc198/D.py
@@ -1,50 +1,43 @@
+from itertools import permutations
+
 S1 = input()
 S2 = input()
 S3 = input()
 
 s = list(set(list(S1 + S2 + S3)))
 d = {c: i for i, c in enumerate(s)}
+D1 = [d[c] for c in S1]
+D2 = [d[c] for c in S2]
+D3 = [d[c] for c in S3]
 if len(s) > 10:
   print("UNSOLVABLE")
   exit()
-  
-def conv(word, m):
-  converted = 0
-  for c in word:
-    converted *= 10
-    converted += m[d[c]]
-    
-  return converted
 
-def solve(n = 0, m = {}, used = set()):
-  if n == len(s):
-    n1 = conv(S1, m)
-    n2 = conv(S2, m)
-    n3 = conv(S3, m)
-    if n1 == 0 or n2 == 0 or n3 == 0:
-      return
-    
-    if len(S1) != len(str(n1)) or len(S2) != len(str(n2)) or len(S3) != len(str(n3)):
-      return
-    
-    if n1 + n2 == n3:
-      print(n1)
-      print(n2)
-      print(n3)
-      exit()
-    
-    return
-  
-  for i in range(10):
-    if i in used:
-      continue
-    
-    m[n] = i
-    used.add(i)
-    solve(n+1, m, used)
-    used.remove(i)
-    del m[n]
-    
-solve()
+
+for perm in permutations(range(10)):
+  n1 = 0
+  for i in D1:
+    n1 = 10 * n1 + perm[i]
+
+  n2 = 0
+  for i in D2:
+    n2 = 10 * n2 + perm[i]
+
+  n3 = 0
+  for i in D3:
+    n3 = 10 * n3 + perm[i]
+
+  if n1 == 0 or n2 == 0 or n3 == 0:
+    continue
+
+  if len(S1) != len(str(n1)) or len(S2) != len(str(n2)) or len(S3) != len(str(n3)):
+    continue
+
+  if n1 + n2 == n3:
+    print(n1)
+    print(n2)
+    print(n3)
+    exit()
+
+
 print("UNSOLVABLE")
-  
