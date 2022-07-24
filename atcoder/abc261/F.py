97885673cd3c63f029268f038dead013cfa1affb
Otsuki Hitoshi <hitochan777@gmail.com>
2022-07-24 15:20:35 +0900

    abc261 F

--- atcoder/abc261/F.py
+++ atcoder/abc261/F.py
@@ -1,41 +1,29 @@
-class FenwickTree:
-    def __init__(self, n):
-        self._n = n
-        self.data = [0] * n
-
-    def add(self, p, x):
-        # assert 0 <= p < self._n
-        p += 1
-        while p <= self._n:
-            self.data[p - 1] += x
-            p += p & -p
-
-    def sum(self, l, r):
-        # assert 0 <= l <= r <= self._n
-        return self._sum(r) - self._sum(l)
-
-    def _sum(self, r):
-        s = 0
-        while r > 0:
-            s += self.data[r - 1]
-            r -= r & -r
-        return s
+from collections import defaultdict
+from atcoder.fenwicktree import FenwickTree
 
 N = int(input())
 C = list(int(x) for x in input().split())
 cs = set(C)
 X = list(int(x) for x in input().split())
-
-fd = {}
-for c in cs:
-  fd[c] = FenwickTree(N+2)
-
-ft = FenwickTree(N+2)
-
+ft = FenwickTree(N+1)
 ans = 0
 for i, x in enumerate(X):
-  ans += ft.sum(x+1, N+1) - fd[C[i]].sum(x+1, N+1)
+  ans += ft.sum(x+1, N+1)
   ft.add(x, 1)
-  fd[C[i]].add(x, 1)
+
+X_by_color = defaultdict(list)
+for x, c in zip(X, C):
+  X_by_color[c].append(x)
+
+for xs in X_by_color.values():
+  # for coordinate compression
+  compress_dict = { x:i for i, x in enumerate(sorted(list(set(xs))))}
+  ft2 = FenwickTree(len(compress_dict))
+  swapped = 0
+  for x in xs:
+    swapped += ft2.sum(compress_dict[x]+1, len(compress_dict)) 
+    ft2.add(compress_dict[x], 1)
+
+  ans -= swapped
 
 print(ans)
