aa10113fdf396518a7653af370760af00df531a9
Otsuki Hitoshi <hitochan777@gmail.com>
2022-07-30 15:25:58 +0900

    ABC229 B

--- atcoder/abc229/B.py
+++ atcoder/abc229/B.py
@@ -1,4 +1,11 @@
 A, B = (int(x) for x in input().split())
 C = str(A + B)
-A = str(A)
-B = str(B)
+A = str(A).zfill(len(C))
+B = str(B).zfill(len(C))
+
+for a, b, c in zip(A, B, C):
+  if int(a) + int(b) < int(c):
+    print("Hard")
+    exit()
+
+print("Easy")
