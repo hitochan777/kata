b6d410c47a928f3cf6e88a44ee4bf5ae78a7e37d
Otsuki Hitoshi <hitochan777@gmail.com>
2022-08-11 23:59:10 +0900

    abc094 D

--- atcoder/abc094/D.py
+++ atcoder/abc094/D.py
@@ -7,10 +7,13 @@ A = list(int(x) for x in input().split())
 A.sort()
 max_val = 0
 ans = None
-for a in A:
+for i, a in enumerate(A):
   if a % 2 == 0:
     idx = bisect.bisect_left(A, a // 2)
     idx = min(idx, len(A)-1)
+    if i == idx:
+      continue
+
     val = comb(a, A[idx])
     if max_val < val:
       max_val = val 
@@ -19,6 +22,9 @@ for a in A:
   else:
     idx1 = bisect.bisect_left(A, a // 2)
     idx1 = min(idx1, len(A)-1)
+    if i == idx1:
+      continue
+
     val = comb(a, A[idx1])
     if max_val < val:
       max_val = val 
@@ -26,6 +32,9 @@ for a in A:
 
     idx2 = bisect.bisect_left(A, a // 2 + 1)
     idx2 = min(idx2, len(A)-1)
+    if i == idx2:
+      continue
+
     val = comb(a, A[idx2])
     if max_val < val:
       max_val = val 
