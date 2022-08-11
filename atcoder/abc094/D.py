1822e4ca310c19acf197bfeebe52fe09dbf2fe32
Otsuki Hitoshi <hitochan777@gmail.com>
2022-08-11 23:54:58 +0900

    abc094 D

---
+++ atcoder/abc094/D.py
@@ -0,0 +1,34 @@
+from math import comb
+
+import bisect
+
+N = int(input())
+A = list(int(x) for x in input().split())
+A.sort()
+max_val = 0
+ans = None
+for a in A:
+  if a % 2 == 0:
+    idx = bisect.bisect_left(A, a // 2)
+    idx = min(idx, len(A)-1)
+    val = comb(a, A[idx])
+    if max_val < val:
+      max_val = val 
+      ans = (a, A[idx])
+
+  else:
+    idx1 = bisect.bisect_left(A, a // 2)
+    idx1 = min(idx1, len(A)-1)
+    val = comb(a, A[idx1])
+    if max_val < val:
+      max_val = val 
+      ans = (a, A[idx1])
+
+    idx2 = bisect.bisect_left(A, a // 2 + 1)
+    idx2 = min(idx2, len(A)-1)
+    val = comb(a, A[idx2])
+    if max_val < val:
+      max_val = val 
+      ans = (a, A[idx2])
+
+print(*ans) 
