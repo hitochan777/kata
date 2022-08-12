41325f5b2d18a322a9bc9acf5308a6cafc6f9fe7
Otsuki Hitoshi <hitochan777@gmail.com>
2022-08-12 16:08:50 +0900

    keyence2020 B

--- atcoder/keyence2020/B.py
+++ atcoder/keyence2020/B.py
@@ -2,14 +2,14 @@ N = int(input())
 robots = []
 for _ in range(N):
   X, L = (int(x) for x in input().split())
-  robots.append((X-L, X+L))
+  robots.append((X+L, X-L))
 
 robots.sort()
 cnt = 0
 prev = -10**18
 for robot in robots:
-  if robot[0] >= prev:
+  if robot[1] >= prev:
     cnt += 1
-    prev = robot[1]
+    prev = robot[0]
 
 print(cnt)
