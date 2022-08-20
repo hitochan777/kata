b74442dbc0f165e273ba5f8b65aff485bfd7d181
Otsuki Hitoshi <hitochan777@gmail.com>
2022-08-20 16:35:17 +0900

    abc264 E

--- atcoder/abc264/E.py
+++ atcoder/abc264/E.py
@@ -21,23 +21,20 @@ lighted = set(range(N, N+M))
 cnt = 0
 for i, (u, v) in enumerate(cables):
   if i in deadX:
-    print("hoge", i)
     continue
 
-  if dsu.same(*cables[X]):
+  if dsu.same(u, v):
     continue
-
-  if (dsu.leader(cables[X][0]) in lighted) != (dsu.leader(cables[X][1]) in lighted):
-    if dsu.leader(cables[X][0]) not in lighted:
-      cnt += dsu.size(cables[X][0])
-      lighted.add(dsu.leader(cables[X][0]))
+  
+  if (dsu.leader(u) in lighted) != (dsu.leader(v) in lighted):
+    if dsu.leader(u) not in lighted:
+      cnt += dsu.size(u)
+      lighted.add(dsu.leader(u))
     else:
-      cnt += dsu.size(cables[X][1])
-      lighted.add(dsu.leader(cables[X][1]))
+      cnt += dsu.size(v)
+      lighted.add(dsu.leader(v))
 
-  dsu.merge(*cables[X])
-
-print(dsu.groups())
+  dsu.merge(u, v)
 
 ans = [cnt]
 for X in Xs:
