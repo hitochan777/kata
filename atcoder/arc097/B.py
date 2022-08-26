5ed3cb45728c1eef78c9aed299a360e5babaf759
Otsuki Hitoshi <hitochan777@gmail.com>
2022-08-26 23:54:40 +0900

    arc97 B

---
+++ atcoder/arc097/B.py
@@ -0,0 +1,16 @@
+from atcoder.dsu import DSU
+
+dsu = DSU(N)
+N, M = (int(x) for x in input().split())
+P = list(int(x)-1 for x in input().split())
+for _ in range(M):
+  x, y = (int(x)-1 for x in input().split())
+  dsu.merge(x, y)
+
+ans = 0
+for group in dsu.groups:
+  s = set(group)
+  ps = set(P[i] for i in s)
+  ans += len(s.intersection(ps))
+
+print(ans)
