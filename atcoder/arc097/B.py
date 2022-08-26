75e792f1048285d87c92b809e419ba9cdfd6859f
Otsuki Hitoshi <hitochan777@gmail.com>
2022-08-26 23:55:18 +0900

    Fix typo

--- atcoder/arc097/B.py
+++ atcoder/arc097/B.py
@@ -1,14 +1,14 @@
 from atcoder.dsu import DSU
 
-dsu = DSU(N)
 N, M = (int(x) for x in input().split())
+dsu = DSU(N)
 P = list(int(x)-1 for x in input().split())
 for _ in range(M):
   x, y = (int(x)-1 for x in input().split())
   dsu.merge(x, y)
 
 ans = 0
-for group in dsu.groups:
+for group in dsu.groups():
   s = set(group)
   ps = set(P[i] for i in s)
   ans += len(s.intersection(ps))
