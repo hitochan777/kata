a797b006f8a98f64bf22d6cafcb120a6efca93c1
Otsuki Hitoshi <hitochan777@gmail.com>
2022-09-08 19:18:41 +0900

    abc267 D

--- atcoder/abc267/D.py
+++ atcoder/abc267/D.py
@@ -8,10 +8,13 @@ def make_array(*args):
   return [make_array(*args[1:]) for _ in range(args[0])]
 
 dp = make_array(N+1, M+1)
-dp[0][0] = 0
+max_val = A[0] 
+for i in range(N):
+  max_val = max(max_val, A[i])
+  dp[i+1][1] = max_val
 
 for i in range(N):
-  for j in range(1, M+1):
+  for j in range(2, M+1):
     dp[i+1][j] = max(dp[i][j], dp[i][j-1] + j * A[i])
 
 print(max(dp[i][M] for i in range(N+1)))
