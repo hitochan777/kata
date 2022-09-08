06a77027efc777439f2c0e00858eb2276285d71e
Otsuki Hitoshi <hitochan777@gmail.com>
2022-09-08 19:11:57 +0900

    abc267 D

--- atcoder/abc267/D.py
+++ atcoder/abc267/D.py
@@ -3,11 +3,12 @@ A = list(int(x) for x in input().split())
 
 def make_array(*args):
   if len(args) == 0:
-    return 0
+    return -10**20
 
   return [make_array(*args[1:]) for _ in range(args[0])]
 
 dp = make_array(N+1, M+1)
+dp[0][0] = 0
 
 for i in range(N):
   for j in range(1, M+1):
