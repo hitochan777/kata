e2b4776ef7556b6a7ca6f62201e35f56c491e376
Otsuki Hitoshi <hitochan777@gmail.com>
2022-09-10 15:03:09 +0900

    abc267 E

---
+++ atcoder/abc267/E.py
@@ -0,0 +1,34 @@
+from collections import defaultdict
+import heapq
+
+
+N, M = (int(x) for x in input().split())
+A = list(int(x) for x in input().split())
+g = defaultdict(list)
+costs = defaultdict(int)
+for _ in range(M):
+  u, v = (int(x)-1 for x in input().split())
+  g[u].append(v)
+  g[v].append(u)
+  costs[u] += A[v]
+  costs[v] += A[u]
+
+h = []
+for node, cost in costs.items():
+  heapq.heappush(h, (cost, node))
+
+ans = 0
+visited = set()
+while len(h) > 0:
+  cost, node = heapq.heappop(h)
+  if node in visited:
+    continue
+
+  visited.add(node)
+  ans = max(ans, cost) 
+  for x in g[node]:
+    costs[x] -= A[node]
+    heapq.heappush(h, (costs[x], x))
+
+print(ans)
+
