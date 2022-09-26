662a504ef47e56a5b58a35378a6434c1d7ae3eb4
Otsuki Hitoshi <hitochan777@gmail.com>
2022-09-26 21:09:52 +0900

    abc270 D

--- atcoder/abc270/D.py
+++ atcoder/abc270/D.py
@@ -1,20 +1,10 @@
 N, K = (int(x) for x in input().split())
 A = list(int(x) for x in input().split())
 
-def make_array(*args):
-  if len(args) == 0:
-    return 0
+dp = [0] * (N+1)
+dp[1] = 1
 
-  return [make_array(*args[1:]) for _ in range(args[0])]
+for i in range(N):
+  dp[i+1] = max(a + i + 1 - a - dp[i+1-a] for a in A if a <= i+1)
 
-dp = make_array(2, N+1)
-
-for i in range(2):
-  for j in range(N+1):
-    for k in range(K):
-      if N - j >= A[k]:
-        dp[i][j] = max(dp[i][j], dp[(i+1)%2][j-A[k]] + A[k])
-
-print(dp[0])
-print(dp[1])
-print(dp[1][N])
+print(dp[N])
