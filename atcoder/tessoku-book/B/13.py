eb13cf0d2afdf62ed6f5cf594aa6ffa019baff4b
Otsuki Hitoshi <hitochan777@gmail.com>
2022-09-28 22:03:14 +0900

    tessoku B13

---
+++ atcoder/tessoku-book/B/13.py
@@ -0,0 +1,19 @@
+N, K = (int(x) for x in input().split())
+A = list(int(x) for x in input().split())
+
+acc = [0]
+for a in A:
+  acc.append(acc[-1]+a)
+
+r = 1
+ans = 0
+for i in range(N):
+  while r < N and acc[r+1] - acc[i] <= K:
+    r += 1
+
+  ans += r - i
+
+print(ans)
+  
+
+
