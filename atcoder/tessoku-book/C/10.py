2d3aa226c74b83427d9182065316a4b102c41ed5
Otsuki Hitoshi <hitochan777@gmail.com>
2022-10-01 15:53:05 +0900

    tessoku C10

--- atcoder/tessoku-book/C/10.py
+++ atcoder/tessoku-book/C/10.py
@@ -1,9 +1,5 @@
 W = int(input())
-dp = [0] * (W+1)
-dp[1] = 12
 MOD = 1000000007
 
-for i in range(1,W):
-  dp[i+1] = (dp[i] * 7) % MOD
-
-print(dp[W])
+# ans = 12 * 7^(W-1)
+print((12 * pow(7, W-1, MOD)) % MOD)
