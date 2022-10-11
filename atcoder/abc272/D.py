de8945256402dc97efb565cf4e4b4dd1a170bb37
Otsuki Hitoshi <hitochan777@gmail.com>
2022-10-11 23:38:54 +0900

    abc272 D

--- atcoder/abc272/D.py
+++ atcoder/abc272/D.py
@@ -1,5 +1,5 @@
 from collections import defaultdict, deque
-
+import sys
 
 N, M = (int(x) for x in input().split())
 
@@ -10,18 +10,18 @@ while i**2 <= M:
   i += 1
 
 g = defaultdict(set)
-for i in range(N):
-  for j in range(N):
-    for y in range(N):
-      s = M - (y-j)**2
-      if s not in squares:
-        continue
+for j in range(N):
+  for y in range(N):
+    s = M - (y-j)**2
+    if s not in squares:
+      continue
 
-      r = squares[s]
-      if 0 <= i - r <= N:
+    r = squares[s]
+    for i in range(N):
+      if 0 <= i - r < N:
         g[(i,j)].add((i-r,y))
 
-      if 0 <= i + r <= N:
+      if 0 <= i + r < N:
         g[(i,j)].add((i+r,y))
 
 
@@ -30,16 +30,18 @@ dist[(0,0)] = 0
 q = deque()
 q.appendleft((0,0,-1))
 visited = set()
+visited.add((0,0))
+cnt = 0
 while len(q):
   i, j, d = q.pop()
-  visited.add((i,j))
   dist[(i,j)] = d + 1
   for ni, nj in g[(i,j)]:
     if (ni, nj) in visited:
       continue
 
+    visited.add((ni,nj))
     q.appendleft((ni,nj,d+1))
 
 for i in range(N):
-  row = [dist[(i,j)] for j in range(N)]
+  row = [dist[(i,j)] if dist[(i,j)] != 10**18 else -1 for j in range(N)]
   print(*row)
