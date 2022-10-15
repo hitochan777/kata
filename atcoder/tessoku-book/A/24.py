dbd85ed29991694c9683b075db9d14bfa7f42e7a
Otsuki Hitoshi <hitochan777@gmail.com>
2022-10-15 17:04:52 +0900

    Tessoku A24

---
+++ atcoder/tessoku-book/A/24.py
@@ -0,0 +1,13 @@
+from bisect import bisect_left
+
+N = int(input())
+A = list(int(x) for x in input().split())
+
+dp = [0] * (N+1)
+L = [10**18] * (N+1)
+L[0] = 0
+for i in range(N):
+  dp[i+1] = bisect_left(L, A[i])
+  L[dp[i+1]] = A[i]
+
+print(dp[N])
