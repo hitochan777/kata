49a977b1d4069145c3bc0e21b6a1329acbab9948
Otsuki Hitoshi <hitochan777@gmail.com>
2022-10-24 00:52:48 +0900

    abc274 E

--- atcoder/abc274/E.py
+++ atcoder/abc274/E.py
@@ -1,5 +1,5 @@
 def dist(p1, p2):
-  return ((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2) ** 0.5
+  return (((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)) ** 0.5
 
 pos = [(0,0)]
 N, M = (int(x) for x in input().split())
@@ -9,25 +9,35 @@ for _ in range(N+M):
 
 N += 1
 
-dp = [[10**18]*(1<<(N+M)) for _ in range(N+M)]
-dp[0][1] = 0
-for s in range(1<<(N+M)):
+INF = 10**20
+
+speeds = [1]*(1<<M)
+for s in range(1<<M):
   speed = 1
-  for i in range(N, N+M):
+  for i in range(M):
     if (s >> i) & 1 == 1:
       speed *= 2
+  
+  speeds[s] = speed
 
+dp = [[INF]*(1<<(N+M)) for _ in range(N+M)]
+dp[0][1] = 0
+for s in range(1<<(N+M)):
+  speed = speeds[s>>N]
   for i in range(N+M):
-    if (s >> i) & 1 == 0:
+    if dp[i][s] == INF:
+      continue
+
+    if not (s >> i) & 1:
       continue
 
     for j in range(N+M):
-      if (s >> j) & 1 == 1:
+      if j != 0 and (s >> j) & 1:
         continue
 
       dp[j][s|(1<<j)] = min(dp[j][s|1<<j], dp[i][s] + dist(pos[i], pos[j]) / speed)
 
-ans = 10**18
+ans = INF 
 for i in range(1<<M):
   ans = min(ans, dp[0][(i<<N)+(1<<N)-1])
 
