6f1ef813d4e55c07d91531c1946ab2701ab78b30
Otsuki Hitoshi <hitochan777@gmail.com>
2022-11-05 14:15:34 +0900

    tessoku B58

---
+++ atcoder/tessoku-book/B/58.py
@@ -0,0 +1,17 @@
+from atcoder.segtree import SegTree
+from bisect import bisect_left
+N, L, R = (int(x) for x in input().split())
+X = list(int(x) for x in input().split())
+
+dp = [10**18] * N
+dp[0] = 0
+
+st = SegTree(lambda x,y: min(x,y), 10**18, dp)
+
+for i in range(1, N):
+  l = bisect_left(X, X[i]-R)
+  r = bisect_left(X, X[i]-L)
+  dp[i] = st.prod(l, r) + 1
+  st.set(i, dp[i])
+
+print(dp[N-1])
