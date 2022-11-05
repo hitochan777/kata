f6151a5a079793919d22cca99744763853f89812
Otsuki Hitoshi <hitochan777@gmail.com>
2022-11-05 14:36:57 +0900

    tessoku B58: fix wrong range in segtree

--- atcoder/tessoku-book/B/58.py
+++ atcoder/tessoku-book/B/58.py
@@ -11,6 +11,12 @@ st = SegTree(lambda x,y: min(x,y), 10**18, dp)
 for i in range(1, N):
   l = bisect_left(X, X[i]-R)
   r = bisect_left(X, X[i]-L)
+  # if X[i] - R < X[l]:
+  #   l += 1
+
+  if X[i] - L < X[r]:
+    r -= 1
+
   dp[i] = st.prod(l, r+1) + 1
   st.set(i, dp[i])
 
