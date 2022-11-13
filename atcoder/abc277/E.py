b541def9d6cc635c96674818c6344665eb612748
Otsuki Hitoshi <hitochan777@gmail.com>
2022-11-13 23:14:56 +0900

    abc277

--- atcoder/abc277/E.py
+++ atcoder/abc277/E.py
@@ -4,31 +4,33 @@ N, M, K = (int(x) for x in input().split())
 g = defaultdict(list)
 for _ in range(M):
   u, v, a = (int(x) for x in input().split())
-  g[u].append((v, a))
-  g[v].append((u, a))
+  u, v = u-1, v-1
+  g[(u, 1-a)].append(((v, 1-a), 1))
+  g[(v, 1-a)].append(((u, 1-a), 1))
 
-S = set(int(x) for x in input().split())
+S = set(int(x)-1 for x in input().split())
+for n in S:
+  g[(n, 0)].append(((n, 1), 0))
+  g[(n, 1)].append(((n, 0), 0))
 
 q = deque()
 visited = set()
-q.append((1, 0, 0))
-visited.add((1, 0))
-dist = [10**18] * (N+1)
+q.append(((0, 0), 0))
+visited.add((0, 0))
+dist = defaultdict(lambda: 10**18)
 while len(q) > 0:
-  n, d, flipped = q.popleft()
-  if n == N:
-    dist[n] = d
-    break
-
-  for nb, a in g[n]:
-    a ^= flipped
-    if a == 0 and n not in S:
+  n, d = q.popleft()
+  dist[n] = d
+  for nb, c in g[n]:
+    if nb in visited:
       continue
 
-    f = flipped if a == 1 else not flipped
-    if (nb, f) not in visited:
-      q.append((nb,d+1,f))
-      visited.add((n, f))
+    if c == 0:
+      q.appendleft((nb,d))
+    else:
+      q.append((nb,d+1))
 
+    visited.add(nb)
 
-print(dist[N] if dist[N] < 10**18 else -1)
+ans = min(dist[(N-1,0)], dist[(N-1, 1)])
+print(ans if ans < 10**18 else -1)
