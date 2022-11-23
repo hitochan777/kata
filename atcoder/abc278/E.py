3de2c76ecb8a28c0be7357d9d12be04bd440308b
Otsuki Hitoshi <hitochan777@gmail.com>
2022-11-23 12:43:58 +0900

    abc278 E

--- atcoder/abc278/E.py
+++ atcoder/abc278/E.py
@@ -1,31 +1,38 @@
 from collections import defaultdict
+import sys
 H, W, N, h, w = (int(x) for x in input().split())
-cnt = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
+# cnt = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
+cnt = [[[0 for _ in range(N + 1)] for _ in range(W + 1)] for _ in range(H + 1)]
 
 A = []
 for i in range(H):
-  a = list(int(x) for x in input().split())
+  a = list(int(x) for x in sys.stdin.readline().split())
   A.append(a)
 
-for i in range(H):
-  for j in range(W):
-    for k, v in cnt[i][j-1].items():
-      cnt[i][j][k] += v
+for i in range(1,H+1):
+  for j in range(1,W+1):
+    cnt[i][j][A[i-1][j-1]] = 1
 
-    cnt[i][j][A[i][j]] += 1
+# print("joge")
 
-for j in range(W):
-  for i in range(H):
-    for k, v in cnt[i-1][j].items():
-      cnt[i][j][k] += v
+for i in range(1,H+1):
+  for j in range(1,W+1):
+    for k in range(1,N+1):
+      cnt[i][j][k] += cnt[i][j-1][k]
 
-for i in range(0, H-h+1):
+for j in range(1,W+1):
+  for i in range(1,H+1):
+    for k in range(1,N+1):
+      cnt[i][j][k] += cnt[i-1][j][k]
+
+# print("done")
+for i in range(1, H-h+2):
   row = []
-  for j in range(0, W-w+1):
+  for j in range(1, W-w+2):
     total = N
     for k in range(1,N+1):
       tmp = cnt[i+h-1][j+w-1][k] - cnt[i+h-1][j-1][k] - cnt[i-1][j+w-1][k] + cnt[i-1][j-1][k]
-      if cnt[H-1][W-1][k] - tmp == 0:
+      if cnt[H][W][k] - tmp == 0:
         total -= 1
 
     row.append(total)
