702c615f8e69c1cd75dc1917a1aac3a51deff0c5
Otsuki Hitoshi <hitochan777@gmail.com>
2022-11-24 23:12:59 +0900

    mayokon20221123 F

--- atcoder/mayokon/20221123/F.py
+++ atcoder/mayokon/20221123/F.py
@@ -9,23 +9,43 @@ for i in range(M):
 
 def solve(g, exclude, N):
   q = deque()
-  q.append((0,0))
+  prevs = [None] * N
+  q.append((0,0, None, None))
   visited = set()
   visited.add(0)
   while len(q) > 0:
-    n, d = q.popleft()
+    n, d, prev, edge = q.popleft()
+    prevs[n] = (prev, edge)
+
     if n == N-1:
-      return d
+      return d, prevs
 
     for i, nb in g[n]:
       if i == exclude or nb in visited:
         continue
 
       visited.add(nb)
-      q.append((nb, d+1))
+      q.append((nb, d+1, n, i))
     
-  return -1
+  return -1, prevs
+
+dist, prevs = solve(g, -1, N)
+if dist == -1:
+  for i in range(M):
+    print(-1)
+
+  exit()
+
+paths = set()
+if dist >= 0:
+  p = N-1
+  while p is not None:
+    p, edge = prevs[p] 
+    paths.add(edge)
 
 for i in range(M):
-  ans = solve(g, i, N)
-  print(ans)
+  if i not in paths:
+    print(dist)
+  else:
+    ans, _ = solve(g, i, N)
+    print(ans)
