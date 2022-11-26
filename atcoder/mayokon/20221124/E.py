bda9355bb375a1b74f154f49badb8bf61a76ec89
Otsuki Hitoshi <hitochan777@gmail.com>
2022-11-26 17:15:42 +0900

    mayokon20221124 E

--- atcoder/mayokon/20221124/E.py
+++ atcoder/mayokon/20221124/E.py
@@ -1,31 +1,30 @@
+INF = 10**18
 H, W = (int(x) for x in input().split())
 g = []
 for _ in range(H):
   S = input()
-  g.append(S+"*")
-
-g.append("*" * (W+1))
+  g.append(S)
 
 def f(c):
   if c == "+":
     return 1
-  elif c == "-":
-    return -1
   else:
-    return 0
+    return -1
 
-dp = [[0] * (W+1) for _ in range(H+1)]
-for i in range(W):
-  dp[H][i] = 10**18 if (H+i)%2==0 else 0
-for i in range(H):
-  dp[i][W] = 10**18 if (W+i)%2==0 else 0
-
+dp = [[0] * W for _ in range(H)]
 for i in range(H-1, -1, -1):
   for j in range(W-1, -1, -1):
+    if i == H-1 and j == W-1:
+      continue
+
     cmp = max if (i + j) % 2 == 0 else min
     sign = 1 if (i+j) % 2 == 0 else -1
-   # print(dp[i+1][j], sign, f(g[i+1][j]), dp[i][j+1], sign, f(g[i][j+1]))
-    dp[i][j] = cmp(dp[i+1][j] + sign*f(g[i+1][j]), dp[i][j+1] + sign*f(g[i][j+1]))
+    # print(dp[i+1][j], sign, f(g[i+1][j]), dp[i][j+1], sign, f(g[i][j+1]))
+    dp[i][j] = -INF if (i+j) % 2 == 0 else INF
+    if i+1 < H:
+      dp[i][j] = cmp(dp[i][j], dp[i+1][j] + sign*f(g[i+1][j]))
+    if j+1 < W:
+      dp[i][j] = cmp(dp[i][j], dp[i][j+1] + sign*f(g[i][j+1]))
 
 # print(dp)
 
