66f9e98430bc71eed8e7b2104255c79de76155ba
Otsuki Hitoshi <hitochan777@gmail.com>
2022-11-27 14:26:34 +0900

    abc279 E

--- atcoder/abc279/E.py
+++ atcoder/abc279/E.py
@@ -1,11 +1,22 @@
-from collections import defaultdict
 N, M = (int(x) for x in input().split())
 A = list(int(x) for x in input().split())
-cnt = defaultdict(list)
-for i, a in enumerate(A):
-  cnt[a].append(i)
+B = list(range(N+1))
 
-for i in range(M):
-  p = 1 
-  for a in 
+for a in A:
+  B[a], B[a+1] = B[a+1], B[a]
+
+pos = {}
+for i, b in enumerate(B):
+  pos[b] = i
+
+B = list(range(N+1))
+for a in A:
+  if B[a] == 1:
+    print(pos[B[a+1]])
+  elif B[a+1] == 1:
+    print(pos[B[a]])
+  else:
+    print(pos[1])
+
+  B[a], B[a+1] = B[a+1], B[a]
 
