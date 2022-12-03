a99255e73ece1be0862a726d3429687126f83b3e
Otsuki Hitoshi <hitochan777@gmail.com>
2022-12-03 17:44:47 +0900

    mayokon 20221201 E

--- atcoder/mayokon/20221201/E.py
+++ atcoder/mayokon/20221201/E.py
@@ -3,12 +3,17 @@ from collections import defaultdict, deque
 sys.setrecursionlimit(10**6)
 N = int(input())
 g = defaultdict(list)
+edges = []
 for _ in range(N-1):
   a, b = (int(x)-1 for x in input().split())
   g[a].append(b)
   g[b].append(a)
+  edges.append((a,b))
 
-def dfs(parents={}, leaves=[], node=0, parent=-1):
+q = [(0,-1)]
+parents = {}
+while len(q) > 0:
+  node, parent = q.pop()
   parents[node] = parent
   children = 0
   for nb in g[node]:
@@ -16,13 +21,30 @@ def dfs(parents={}, leaves=[], node=0, parent=-1):
       continue
 
     children += 1
-    dfs(parents, leaves, nb, node)
+    q.append((nb, node))
 
-  if children == 0:
-    leaves.append(node)
+scores = defaultdict(int)
+Q = int(input())
+for _ in range(Q):
+  t, e, x = (int(x) for x in input().split())
+  avoid = edges[e-1][t%2]
+  node = edges[e-1][(t+1)%2]
+  if parents[node] == avoid:
+    scores[node] += x
+  else:
+    scores[0] += x
+    scores[avoid] -= x
 
-parents = {}
-leaves = []
-dfs(parents, leaves)
+q = deque()
+q.append((0, scores[0]))
+while len(q) > 0:
+  node, score = q.popleft()
+  scores[node] = score
+  for nb in g[node]:
+    if nb == parents[node]:
+      continue
 
-print(parents, leaves)
+    q.append((nb, score+scores[nb]))
+
+for i in range(N):
+  print(scores[i])
