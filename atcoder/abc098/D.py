cbacbd166161d32dd5b7dab176ab8fe0bf5d6a00
Otsuki Hitoshi <hitochan777@gmail.com>
2022-12-03 15:58:43 +0900

    abc098 D

--- atcoder/abc098/D.py
+++ atcoder/abc098/D.py
@@ -7,11 +7,13 @@ for a in A:
   axor.append(axor[-1]^a)
   asum.append(asum[-1]+a)
 
-r = 0
+r = 1
 ans = 0
 for l in range(N):
-  r = l
   while r < N and (asum[r+1] - asum[l]) == (axor[r+1] - axor[l]):
+    r += 1
+
+  if r == l:
     r += 1
 
   ans += r - l
