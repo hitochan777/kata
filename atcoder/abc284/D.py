96fa266ba098896429aca86116139e538a18f97b
Otsuki Hitoshi <hitochan777@gmail.com>
2023-01-10 23:44:18 +0900

    abc284 D

--- atcoder/abc284/D.py
+++ atcoder/abc284/D.py
@@ -1,3 +1,5 @@
+import math
+
 def get_primes(n):
   ok = [True]*(n+1)
   ok[0] = False
@@ -6,17 +8,25 @@ def get_primes(n):
     if not ok[p]:
       continue
 
-    for q in range(p, n+1, p):
+    for q in range(p*2, n+1, p):
       ok[q] = False
 
-  return ok
-    
+  return [i for i in range(n+1) if ok[i]]
+
+def is_square(i: int) -> bool:
+    return i == math.isqrt(i) ** 2
 
 T = int(input())
-primes = set(get_primes(10**7))
+primes = set(get_primes(3*10**6+1))
+# print(primes)
 for _ in range(T):
   N = int(input())
   for n in primes:
-    if N % n == 0:
-      N // n
+    if N % (n**2) == 0:
+      print(n, N // (n**2))
+      break
+    
+    if N % n == 0 and is_square(N//n):
+      print(math.isqrt(N//n), n)
+      break
 
