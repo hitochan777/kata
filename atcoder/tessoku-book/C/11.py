f617d3ffeb46db3509c04c14250f1e5987cad9e9
Otsuki Hitoshi <hitochan777@gmail.com>
2023-01-30 21:50:02 +0900

    tessoku C11

---
+++ atcoder/tessoku-book/C/11.py
@@ -0,0 +1,14 @@
+N, K = (int(x) for x in input().split())
+A = [int(x) for x in input().split()]
+
+ng, ok = 0, 10**9+1
+while ok-ng > (10**(-6)):
+    m = (ok+ng) / 2
+    k = sum(a//m for a in A)
+    if k <= K:
+        ok = m
+    else:
+        ng = m
+
+ans = list(int(a//ok) for a in A)
+print(*ans)
