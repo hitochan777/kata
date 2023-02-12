dc301ded00a2d68bcc4ca201a021f6477231832b
Otsuki Hitoshi <hitochan777@gmail.com>
2023-02-12 15:55:45 +0900

    abc289 E improve

--- atcoder/abc289/E.py
+++ atcoder/abc289/E.py
@@ -1,6 +1,5 @@
 from collections import defaultdict, deque 
 import sys
-from atcoder.dsu import DSU
 
 input = sys.stdin.readline
 T = int(input())
@@ -10,40 +9,31 @@ def solve():
   q = deque()
   N, M = (int(x) for x in input().split())
   C = list(int(x) for x in input().split())
-  dsu = DSU(N)
   for _ in range(M):
     u, v = (int(x)-1 for x in input().split())
     g[u].append(v)
     g[v].append(u)
-    dsu.merge(u, v)
 
-  if not dsu.same(0, N-1):
-    return -1
-
-  ans = 10**18
   q.appendleft((0, N-1, 0))
-  visited = set() 
+  visited = set()
   while len(q) > 0:
     n, m, d = q.pop()
     if n == N-1 and m == 0:
-      ans = min(ans, d)
-      return ans
+      return d
 
     for n2 in g[n]:
       for m2 in g[m]:
-        if (n2, m2) in visited:
+        key = n2 * 10000 + m2
+        # using tuple as key make set operation slow
+        # key = (n2, m2)
+        if C[n2] == C[m2] or key in visited:
           continue
 
-        visited.add((n2,m2))
-        if C[n2] == C[m2]:
-          continue
+        visited.add(key)
 
         q.appendleft((n2, m2, d+1))
 
-  if ans == 10**18:
-    return -1
-  else:
-    return ans
+  return -1
 
 for _ in range(T):
   ans = solve()
