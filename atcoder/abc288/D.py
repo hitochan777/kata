7d5623c444b5c03b0f521419a2d6dfe30ead004f
Otsuki Hitoshi <hitochan777@gmail.com>
2023-02-19 16:47:53 +0900

    abc288 D

--- atcoder/abc288/D.py
+++ atcoder/abc288/D.py
@@ -1,26 +1,17 @@
 N, K = (int(x) for x in input().split())
-arr = [int(x) for x in input().split()]
+A = [int(x) for x in input().split()]
 
-def is_good(A, K):
-    n = len(A)
-    for i in range(n-K+1):
-        offset = A[i]
-        for j in range(K):
-            A[i+j] -= offset
-
-    return all(a == 0 for a in A)
+acc = [[0] for _ in range(K)]
+for i in range(N):
+  for k in range(K):
+    acc[k].append(acc[k][-1]+(A[i] if i % K == k else 0))
 
 Q = int(input())
 for _ in range(Q):
-  l, r = (int(x)-1 for x in input().split())
-  if is_good(arr[l:r+1], K):
-      print("Yes")
+  l, r = (int(x) for x in input().split())
+  base = acc[0][r] - acc[0][l-1]
+  if all(acc[i][r] - acc[i][l-1] == base for i in range(1, K)):
+    print("Yes")
   else:
-      print("No")
-        
-# 16 18 33 32 28 26 11 12
-# 0  2  17 32 28 26 11 12
-# 0  0  15 30 28 26 11 12
-# 0  0  0  15 13 26 11 12
-# 0  0  0  0  -2 11 11 12
-# 0  0  0  0  0  13 13 12
+    print("No")
+  
