42a3925b47c59222e0b34720c1449db0642a5087
Otsuki Hitoshi <hitochan777@gmail.com>
2023-03-19 16:45:19 +0900

    abc290 D

--- atcoder/abc290/D.py
+++ atcoder/abc290/D.py
@@ -1,18 +1,12 @@
-import math
+from math import gcd
+
 T = int(input())
 for _ in range(T):
   N, D, K = (int(x) for x in input().split())
-  D %= N
-  if D == 0:
-    print(K-1)
-  elif D == N-1:
-    if K == 1:
-      print(0)
-    elif K == 2:
-      print(N-1)
-    else:
-      print(N-2-(K-3))
-  else:
-    C = math.ceil(N / D)
-    ans = (K-1) // C + ((K-1) % C) * D
-    print(ans)
+  K -= 1
+  g = gcd(N, D)
+  Ng = N // g
+  Dg = D // g 
+  ans = ((K * Dg) % Ng) * g + (K // Ng)
+  print(ans)
+  
