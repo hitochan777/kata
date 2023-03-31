69db5d260a082353336d3530533ed364b8a46cca
Otsuki Hitoshi <hitochan777@gmail.com>
2023-03-31 23:57:39 +0900

    asakatsu 20230329 E

--- atcoder/asakatsu/20230329/E.py
+++ atcoder/asakatsu/20230329/E.py
@@ -1,30 +1,11 @@
-# 2 3
-# 3 3
-# 3 2
-# 6 4
-
-# t = T / A * a
-
 import math
 N = int(input())
 t, a = 1, 1
 for _ in range(N):
     T, A = (int(x) for x in input().split())
-    if T >= t:
-      if A >= a:
-        t = T
-        a = A
-      else:
-        a = math.ceil(a / A) * A
-        t = T * a // A
-    else: # T < t
-      if A >= a:
-        t = math.ceil(t / T) * T
-        a = A * t // T
-      else:
-        a, t = math.ceil(a / A) * A, T * a // A
-
-    print(t,a )
+    n = max(math.ceil(t/T), math.ceil(a/A))
+    t = n * T
+    a = n * A
 
 print(t+a)
 
