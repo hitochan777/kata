f961f5526ab3934656a1e4bbc7e3f5b1ab5c2ad0
Otsuki Hitoshi <hitochan777@gmail.com>
2023-04-12 00:02:07 +0900

    mayokon 20230411 B

--- atcoder/mayokon/20230411/B.py
+++ atcoder/mayokon/20230411/B.py
@@ -1,8 +1,12 @@
 from collections import Counter
 N = int(input())
 A = list(int(x) for x in input().split())
-cnt = Counter(A).items()
+cnt = list(Counter(A).items())
+ans = 0
 for i in range(len(cnt)):
     k1, v1 = cnt[i]
     for j in range(i+1, len(cnt)):
       k2, v2 = cnt[j]
+      ans += ((k1 - k2)**2)*(v1*v2)
+
+print(ans)
