2c173b6e786bb4457d69ac5c2529cd8aeaef1e2e
Otsuki Hitoshi <hitochan777@gmail.com>
2023-05-27 13:59:22 +0900

    abc302 F

--- atcoder/abc302/F.py
+++ atcoder/abc302/F.py
@@ -1,8 +1,6 @@
 from collections import defaultdict, deque
 N, M = (int(x) for x in input().split())
-ss = set()
-ones = set()
-ms = set()
+ss = []
 for _ in range(N):
   _ = int(input())
   S = set(int(x) for x in input().split())
@@ -10,48 +8,29 @@ for _ in range(N):
      print(0)
      exit()
 
-  ss.add(frozenset(S))
+  ss.append(S)
 
 g = defaultdict(set)
-ss = list(ss)
-contains = defaultdict(set)
 for i, s in enumerate(ss):
-  if 1 in s:
-    ones.add(i)
-
-  if M in s:
-    ms.add(i)
-
   for a in s:
-    contains[a].add(i)
+    b = N+a-1
+    g[i].add(b)
+    g[b].add(i)
 
-for s in contains.values():
-  for i in s:
-     for j in s:
-        if i == j:
-           continue
-
-        g[i].add(j)
-        g[j].add(i)
-
+q = deque()
+q.appendleft((N, 0))
+visited = set()
+visited.add(N)
 ans = 10**18
-for i in ones:
-  q = deque()
-  q.appendleft((i, 0))
-  visited = set()
-  visited.add(i)
-  while q:
-    node, d = q.pop()
-    if node in ms:
-       ans = min(ans, d)
-       break
+while q:
+  node, d = q.pop()
+  if node == N + M - 1:
+    print(d // 2 - 1)
+    exit()
 
-    for nb in g[node]:
-       if nb not in visited:
-          visited.add(nb)
-          q.appendleft((nb, d+1))
+  for nb in g[node]:
+    if nb not in visited:
+      visited.add(nb)
+      q.appendleft((nb, d+1))
 
-if ans == 10**18:
-   print(-1)
-else:
-  print(ans)
+print(-1)
