302b4f7cb58bf6711f857be14b9f913712ac1613
Otsuki Hitoshi <hitochan777@gmail.com>
2023-07-27 00:06:56 +0900

    abc311 D

--- atcoder/abc311/D.py
+++ atcoder/abc311/D.py
@@ -6,18 +6,20 @@ for _ in range(N):
 
 q = [(1,1,0,0)]
 visited = set()
+visited2 = set()
 dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 while q:
   x, y, dx, dy = q.pop()
   visited.add((x, y))
+  visited2.add((x, y, dx, dy))
   if dx == 0 and dy == 0:
     for dx2, dy2 in dirs:
       nx, ny = x + dx2, y + dy2
-      if g[nx][ny] == "." and (nx, ny) not in visited:
+      if g[nx][ny] == "." and (nx, ny, dx2, dy2) not in visited2:
         q.append((nx, ny, dx2, dy2))
   else:
     nx, ny = x + dx, y + dy
-    if g[nx][ny] == "." and (nx, ny) not in visited:
+    if g[nx][ny] == "." and (nx, ny, dx, dy) not in visited2:
       q.append((nx, ny, dx, dy))
     elif g[nx][ny] == "#":
       q.append((x, y, 0, 0))
