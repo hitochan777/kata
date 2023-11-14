4189c86877e78f57b9ff966ec58905e0744f8e77
Otsuki Hitoshi <hitochan777@gmail.com>
2023-11-15 00:02:38 +0900

    abc328 B

--- atcoder/abc/abc328/B.py
+++ atcoder/abc/abc328/B.py
@@ -2,11 +2,9 @@ N = int(input())
 D = list(int(x) for x in input().split())
 
 ans = 0
-for i, jd in enumerate(D, start=1):
+for i, d in enumerate(D, start=1):
   for v in range(1, d):
-    if len(set(list(f"{i}{v}"))):
-      ans #
+    if len(set(list(f"{i}{v}"))) == 1:
+      ans += 1
 
-
-
-::
+print(ans)
